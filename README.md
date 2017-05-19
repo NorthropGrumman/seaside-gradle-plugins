288d872f81aa0ef18e27b6feab7a7b7a43167444
provencej <justan.provence@ngc.com>
2017-05-19 12:02:20 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# seaside-gradle-parent
-The gradle plugin that all seaside projects will use as a base build
+# The seaside parent gradle build plugin.
+The gradle plugin that all seaside projects will use to provide a common build structure.
+
+# Features
+The seaside gradle parent plugin provides a base gradle build for all seaside projects. This plugin will ensure your
+bundles are named correctly, provide an OSGi enabled jar file (bundle), generate javadocs jar, generate sources jar
+and provide the default configuration for deploying these artifacts to Nexus.
+
+## This plugin requires properties in your gradle.properties file (usually ~/.gradle/gradle.properties):
+* nexusUsername     : the username to use when uploading artifacts to nexus
+* nexusPassword     : the password to use when uploading artifacts to nexus
+* nexusReleases     : url to the releases repository
+* nexusSnapshots    : url to the snapshots repository
+* nexusConsolidated : url to the maven public download site usually a proxy to maven central and the
+releases and snapshots
+
+# Using this plugin
+To use the plugin you will need to add the classpath to your buildscript dependencies and then just apply the plugin.
+An example is below. Note: a newer version may exist. Check the Nexus repository for the latest version.
+
+```java
+buildscript {
+    repositories {
+        mavenLocal()
+
+        maven {
+            url nexusConsolidated
+        }
+    }
+
+    dependencies {
+        classpath 'com.ngc.seaside:seaside.parent:1.0'
+    }
+}
+
+subprojects {
+    apply plugin: 'com.ngc.seaside.parent'
+
+    group = 'com.ngc.seaside'
+    version = '1.0-SNAPSHOT'
+
+}
+```
---
+++ gradle.plugin/src/main/groovy/com/ngc/blocs/gradle/plugin/seaside/parent/SeasideParentPlugin.groovy
@@ -0,0 +1,169 @@
+package com.ngc.blocs.gradle.plugin.seaside.parent
+
+import aQute.bnd.gradle.BundleTaskConvention
+import org.gradle.api.JavaVersion
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.bundling.Jar
+
+/**
+ * The seaside parent plugin provides calls to common task, sets up the default dependencies for BLoCS and OSGi along
+ * with providing nexus repository deployment settings.
+ *
+ * The following properties are required in your ~/.gradle/gradle.properties file to use this plugin.
+ * <pre>
+ *     nexusUsername     : the username to use when uploading artifacts to nexus
+ *     nexusPassword     : the password to use when uploading artifacts to nexus
+ *     nexusReleases     : url to the releases repository
+ *     nexusSnapshots    : url to the snapshots repository
+ *     nexusConsolidated : url to the maven public download site
+ *                         usually a proxy to maven central and the releases and snapshots
+ *
+ * </pre>
+ *
+ * To use this plugin in your gradle.build :
+ * <pre>
+ *    buildscript {
+ *       repositories {
+ *           mavenLocal()
+ *
+ *            maven {
+ *              url nexusConsolidated
+ *            }
+ *        }
+ *
+ *        dependencies {
+ *             classpath 'com.ngc.seaside:seaside.parent:1.0'
+ *        }
+ *     }
+ *
+ *     subprojects {
+ *
+ *        apply plugin: 'seaside.parent'
+ *
+ *        group = 'com.ngc.seaside'
+ *        version = '1.0-SNAPSHOT'
+ *     }
+ * </pre>
+ */
+class SeasideParentPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project p) {
+        p.configure(p) {
+
+            /**
+             * This plugin requires the java and maven plugins
+             */
+            plugins.apply 'java'
+            plugins.apply 'maven'
+
+            /**
+             * Create a task for generating the source jar. This will also be uploaded to Nexus.
+             */
+            task('sourcesJar', type: Jar, dependsOn: [classes]) {
+                classifier = 'sources'
+                from sourceSets.main.allSource
+            }
+
+            /**
+             * Create a task for generating the javadoc jar. This will also be uploaded to Nexus.
+             */
+            task('javadocJar', type: Jar, dependsOn: [classes, javadoc]) {
+                classifier = 'javadoc'
+                from javadoc.destinationDir
+            }
+
+            afterEvaluate {
+                /**
+                 * Ensure to add the doclint option to the javadoc task if using Java 8.
+                 */
+                if (JavaVersion.current().isJava8Compatible()) {
+                    tasks.getByName('javadoc') { doc ->
+                        options.addStringOption('Xdoclint:none', '-quiet')
+                    }
+                }
+
+                /**
+                 * Add the standard repositories. All of the seaside content should be downloaded from
+                 * the nexus consolidated repository.
+                 */
+                repositories {
+                    mavenLocal()
+
+                    maven {
+                        url nexusConsolidated
+                    }
+                }
+
+                /**
+                 *
+                 */
+                ext {
+                    // The default name of the bundle.
+                    bundleName = "$group" + '.' + "$project.name"
+                }
+
+                /**
+                 * Augment the jar name to be $groupId.$project.name).
+                 */
+                tasks.getByName('sourcesJar') { jar ->
+                    archiveName = "${project.group}.${project.name}-${project.version}-${classifier}.jar"
+                }
+
+                tasks.getByName('javadocJar') { jar ->
+                    archiveName = "${project.group}.${project.name}-${project.version}-${classifier}.jar"
+                }
+
+                /**
+                 * Augment the jar to be OSGi compatible.
+                 */
+                tasks.getByName('jar') { jar ->
+                    convention.plugins.bundle = new BundleTaskConvention(jar)
+
+                    archiveName = "${project.group}.${project.name}-${project.version}.jar"
+
+                    doLast {
+                        buildBundle()
+                    }
+
+                    manifest {
+                        attributes('Bundle-Name': "$bundleName",
+                                   'Bundle-SymbolicName': "$bundleName")
+                    }
+                }
+
+                /**
+                 * The nexus upload configuration.
+                 */
+                uploadArchives {
+                    repositories {
+                        mavenDeployer {
+                            // Use the main repo for full releases.
+                            repository(url: nexusReleases) {
+                                // Make sure that nexusUsername and nexusPassword are in your
+                                // ${gradle.user.home}/gradle.properties file.
+                                authentication(userName: nexusUsername, password: nexusPassword)
+                            }
+                            // If the version has SNAPSHOT in the name, use the snapshot repo.
+                            snapshotRepository(url: nexusSnapshots) {
+                                authentication(userName: nexusUsername, password: nexusPassword)
+                            }
+                        }
+                    }
+                }
+
+                /**
+                 * Ensure we call the 2 new tasks for generating the javadoc and sources artifact jars.
+                 */
+                artifacts {
+                    archives sourcesJar
+                    archives javadocJar
+                }
+
+            }
+
+            defaultTasks = ['build']
+        }
+    }
+}
