a22545f2730490f4716129b543917131df72b35f
Alexander Monnet <Alexander.Monnet@ngc.com>
2017-07-18 16:16:38 -0500

    Initial plugin extension

--- com.ngc.seaside.gradle.plugins/src/main/groovy/com/ngc/seaside/gradle/plugins/application/SeasideApplicationPlugin.groovy
+++ com.ngc.seaside.gradle.plugins/src/main/groovy/com/ngc/seaside/gradle/plugins/application/SeasideApplicationPlugin.groovy
@@ -8,38 +8,57 @@ import org.gradle.api.Project
  */
 class SeasideApplicationPlugin implements Plugin<Project> {
 
-    @Override
-    void apply(Project p) {
-        p.configure(p) {
+   @Override
+   void apply(Project p) {
+      p.configure(p) {
 
-            /**
-             * This plugin requires the java and maven plugins
-             */
-            plugins.apply 'java'
-            plugins.apply 'application'
+         extensions.create("seasideApplication", SeasideApplicationPluginExtension)
 
-            /**
-             * Modify distZip task to include resources
-             */
-            distZip {
+         /**
+          * This plugin requires the java and maven plugins
+          */
+         plugins.apply 'java'
+         plugins.apply 'application'
 
+         /**
+          * Modify distZip task to include resources
+          */
+         distZip {
+
+         }
+
+         /**
+          * Modify distZip task to include resources
+          */
+         distTar {
+
+         }
+
+         /**
+          * Modify start scripts task to allow custom start scripts
+          */
+         startScripts {
+            def scriptString = (String) seasideApplication.startScript
+            shell('echo ' + scriptString)
+            if (scriptString != null) {
+               //do special script
+               def startScript = new File(scriptString )
+               def scriptsDirectory = startScript.getParentFile()
+               println(scriptsDirectory.toString())
+               scriptsDirectory.listFiles().each { file ->
+                  if(file.name == startScript.name){ //not going to work for .sh scripts missing the .sh
+                     //change name then copy
+                     println(startScript.name)
+                  } else {
+                     //copy file
+                  }
+               }
+            } else {
+               //super.startScripts
             }
+         }
 
-            /**
-             * Modify distZip task to include resources
-             */
-            distTar {
-
-            }
-
-            /**
-             * Modify start scripts task to allow custom start scripts
-             */
-            startScripts {
-
-            }
-
-            defaultTasks = ['build']
-        }
-    }
+         defaultTasks = ['build']
+      }
+   }
 }
