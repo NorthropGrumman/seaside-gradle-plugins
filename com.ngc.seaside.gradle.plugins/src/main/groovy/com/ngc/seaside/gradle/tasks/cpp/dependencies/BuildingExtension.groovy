045bc8bbda4d6d0e985785dfcadb46d06a7ef687
provencej <justan.provence@ngc.com>
2017-09-28 09:50:26 -0500

    Added overloaded methods for statically

--- com.ngc.seaside.gradle.plugins/src/main/groovy/com/ngc/seaside/gradle/tasks/cpp/dependencies/BuildingExtension.groovy
+++ com.ngc.seaside.gradle.plugins/src/main/groovy/com/ngc/seaside/gradle/tasks/cpp/dependencies/BuildingExtension.groovy
@@ -17,14 +17,26 @@ class BuildingExtension {
         this.project = project
     }
 
+    void statically(String dependencyName) {
+        statically(dependencyName, null)
+    }
+
     void statically(Closure closure) {
+        statically("", closure)
+    }
+
+    void statically(String dependencyName, Closure closure) {
         Statically statically = new Statically(this.project)
-        project.configure(statically, closure)
+        statically.dependency = dependencyName
+        if (closure != null) {
+            project.configure(statically, closure)
+        }
         staticList.add(statically)
         storage.add(statically)
     }
 
     static class Statically {
+
         Project project
         String dependency
         List<String> libs = []
@@ -52,6 +64,7 @@ class BuildingExtension {
     }
 
     static class StaticWithArgs {
+
         String before
         String after
 
