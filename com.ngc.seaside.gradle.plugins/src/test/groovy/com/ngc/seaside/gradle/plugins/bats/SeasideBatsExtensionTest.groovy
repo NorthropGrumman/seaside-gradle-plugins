c20875807d75a1edc434c8060a31fab33487a6d8
Cameron Chaparro <cameron.chaparro@ngc.com>
2017-09-29 14:54:14 -0500

    Update README.md
    Update the README file to include usage information, etc for the
    SeasideBatsPlugin
    
    Also, fixed some indentation issues in the SeasideBatsExtensionTest file

--- com.ngc.seaside.gradle.plugins/src/test/groovy/com/ngc/seaside/gradle/plugins/bats/SeasideBatsExtensionTest.groovy
+++ com.ngc.seaside.gradle.plugins/src/test/groovy/com/ngc/seaside/gradle/plugins/bats/SeasideBatsExtensionTest.groovy
@@ -25,32 +25,32 @@ class SeasideBatsExtensionTest {
    @Test
    void returnsCorrectPathToTheBatsScript() {
       def expected = [
-               project.buildDir.name,
-               "tmp",
-               "bats-scripts",
-               "bats-${extension.BATS_VERSION}",
-               "libexec",
-               "bats" ].join(File.separator)
+            project.buildDir.name,
+            "tmp",
+            "bats-scripts",
+            "bats-${extension.BATS_VERSION}",
+            "libexec",
+            "bats" ].join(File.separator)
       Assert.assertEquals(expected, extension.BATS_PATHS.PATH_TO_THE_BATS_SCRIPT)
    }
 
    @Test
    void returnsCorrectPathToTheDefaultDirectoryWithTests() {
       def expected = [
-               "src",
-               "test",
-               "resources",
-               "bats-plugin" ].join(File.separator)
+            "src",
+            "test",
+            "resources",
+            "bats-plugin" ].join(File.separator)
       Assert.assertEquals(expected, extension.BATS_PATHS.DIRECTORY_WITH_BATS_TESTS)
    }
 
    @Test
    void returnsCorrectPathToTheDefaultTestResultsFile() {
       def expected = [
-               project.buildDir.name,
-               "test-results",
-               "bats-tests",
-               "results.out" ].join(File.separator)
+            project.buildDir.name,
+            "test-results",
+            "bats-tests",
+            "results.out" ].join(File.separator)
       Assert.assertEquals(expected, extension.BATS_PATHS.BATS_TEST_RESULTS_FILE)
    }
 }
