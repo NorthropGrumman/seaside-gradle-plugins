0680c76ee4d30dd680348435fe0831b3a10d29d1
Cameron Chaparro <cameron.chaparro@ngc.com>
2017-10-18 12:11:31 -0500

    Fix the bug in the functional tests
    As it turns out, this was an issue due to the way testing differs from
    running a build from a project. Specifically, adding an afterEvaluate
    section to the plugin and putting the dependency on lcov inside there
    cleaned this problem up by ensuring the dependency was present before
    reaching the execution stage of the build. This has the added bonus of
    allowing us to specify the lcov version in one place (i.e. the plugin's
    extension).

--- com.ngc.seaside.gradle.plugins/src/main/groovy/com/ngc/seaside/gradle/plugins/util/test/TestingUtilities.groovy
+++ com.ngc.seaside.gradle.plugins/src/main/groovy/com/ngc/seaside/gradle/plugins/util/test/TestingUtilities.groovy
@@ -1,13 +1,28 @@
 package com.ngc.seaside.gradle.plugins.util.test
 
+import java.nio.file.Paths
+
 class TestingUtilities {
+    static List<File> getTestClassPath(Class c) {
+        URL r = getThePluginClassPathResource(c)
+        throwIfTheClasspathResourceIsNotFound(r)
+        return createNewFileForEachItemInClasspath(r)
+    }
 
-    static List<File> getTestClassPath(Class clazz) {
-        URL pluginClasspathResource = clazz.classLoader.getResource("plugin-classpath.txt")
-        if (pluginClasspathResource == null) {
+    private static URL getThePluginClassPathResource(Class c) {
+        return c.classLoader.getResource("plugin-classpath.txt")
+    }
+
+    private static void throwIfTheClasspathResourceIsNotFound(URL r) {
+        if (!r)
             throw new IllegalStateException("Did not find plugin classpath resource, run `testClasses` build task.")
-        }
+    }
 
-        return pluginClasspathResource.readLines().collect { new File(it) }
+    private static List<File> createNewFileForEachItemInClasspath(URL r) {
+        return r.readLines().collect { new File(it) }
+    }
+
+    static File turnListIntoPath(String... list) {
+        return Paths.get(list.flatten().join(File.separator)).toFile()
     }
 }
