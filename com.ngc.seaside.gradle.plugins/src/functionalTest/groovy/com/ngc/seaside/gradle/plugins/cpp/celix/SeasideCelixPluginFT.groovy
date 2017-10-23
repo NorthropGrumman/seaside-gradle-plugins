35094e6e6da7f0da4f66ddbe78c793f14a91833a
ThomasJHooper <thomas.hooper@ngc.com>
2017-10-23 08:44:21 -0500

    Add functional test. :

---
+++ com.ngc.seaside.gradle.plugins/src/functionalTest/groovy/com/ngc/seaside/gradle/plugins/cpp/celix/SeasideCelixPluginFT.groovy
@@ -0,0 +1,46 @@
+package com.ngc.seaside.gradle.plugins.cpp.celix
+
+import com.ngc.seaside.gradle.plugins.util.test.TestingUtilities
+import org.apache.commons.io.FileUtils
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
+class SeasideCelixPluginFT {
+
+    private File projectDir
+    private Project project
+    private List<File> pluginClasspath
+
+    @Before
+    void before() {
+        pluginClasspath = TestingUtilities.getTestClassPath(getClass())
+
+        File source = Paths.get("src/functionalTest/resources/pipeline-test-cpp").toFile()
+        Path targetPath = Paths.get("build/functionalTest/cpp/parent/pipeline-test-cpp")
+        projectDir = Files.createDirectories(targetPath).toFile()
+        FileUtils.copyDirectory(source, projectDir)
+
+        project = ProjectBuilder.builder().withProjectDir(projectDir).build()
+    }
+
+    @Test
+    void doesRunGradleBuildWithSuccess() {
+        BuildResult result = GradleRunner.create().withProjectDir(projectDir)
+              .withPluginClasspath(pluginClasspath)
+              .forwardOutput()
+              .withArguments("clean", "build")
+              .build()
+
+        Assert.assertEquals(TaskOutcome.valueOf("SUCCESS"), result.task(":service.event.impl.synceventservice:build").getOutcome())
+    }
+}
