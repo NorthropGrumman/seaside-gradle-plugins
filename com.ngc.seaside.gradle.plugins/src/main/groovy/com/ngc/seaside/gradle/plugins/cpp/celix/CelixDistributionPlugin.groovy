cbf283f906c4804471b6928a52fe08d9d8c91afe
ThomasJHooper <thomas.hooper@ngc.com>
2017-10-23 08:09:59 -0500

    Configure copy tasks for Celix Distribution plugin.

--- com.ngc.seaside.gradle.plugins/src/main/groovy/com/ngc/seaside/gradle/plugins/cpp/celix/CelixDistributionPlugin.groovy
+++ com.ngc.seaside.gradle.plugins/src/main/groovy/com/ngc/seaside/gradle/plugins/cpp/celix/CelixDistributionPlugin.groovy
@@ -2,8 +2,11 @@ package com.ngc.seaside.gradle.plugins.cpp.celix
 
 import com.ngc.seaside.gradle.extensions.cpp.celix.CelixDistributionExtension
 import com.ngc.seaside.gradle.plugins.util.GradleUtil
+import com.ngc.seaside.gradle.plugins.util.TaskResolver
+import org.apache.commons.io.FilenameUtils
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Copy
 
 class CelixDistributionPlugin implements Plugin<Project> {
 
@@ -14,7 +17,9 @@ class CelixDistributionPlugin implements Plugin<Project> {
     public static final String COPY_BUNDLES_TASK_NAME = "copyBundles"
     public static final String CREATE_RUN_SCRIPT_TASK_NAME = "createRunScript"
     public static final String CREATE_DISTRIBUTION_ZIP_TASK_NAME = "createDistributionZip"
-    public static final String DISTRIBUTION_TASK_GROUP = "Celix Distribution"
+
+    public static final String DISTRIBUTION_TASK_GROUP_NAME = "Celix Distribution"
+
     public static final String EXTENSION_NAME = "celixDistribution"
 
     private CelixDistributionExtension extension
@@ -30,10 +35,8 @@ class CelixDistributionPlugin implements Plugin<Project> {
                                        'nexusUsername',
                                        'nexusPassword')
 
-            extension = p.extensions.create(EXTENSION_NAME, CelixDistributionExtension)
-
+            registerExtensions(p)
             applyPlugins(p)
-            createDefaultProperties(p)
             createConfigurations(p)
             createTasks(p)
 
@@ -53,6 +56,10 @@ class CelixDistributionPlugin implements Plugin<Project> {
         GradleUtil.requireProperties(project.properties, propertyName, propertyNames)
     }
 
+    protected void registerExtensions(Project project) {
+        extension = project.extensions.create(EXTENSION_NAME, CelixDistributionExtension)
+    }
+
     protected void applyPlugins(Project project) {
         project.getPlugins().apply('maven')
     }
@@ -66,6 +73,34 @@ class CelixDistributionPlugin implements Plugin<Project> {
     }
 
     protected void createTasks(Project project) {
+        project.task(COPY_CELIX_TASK_NAME,
+                     group: DISTRIBUTION_TASK_GROUP_NAME,
+                     type: Copy,
+                     description: 'Copies the Celix ZIP artifact to the build directory.') {
+            from project.configurations.getByName("celix")
+            into { "${project.buildDir}" }
+            rename { name -> "celix.zip" }
+        }
 
+        project.task(UNPACK_CELIX_TASK_NAME,
+                     group: DISTRIBUTION_TASK_GROUP_NAME,
+                     type: Copy,
+                     description: 'Extracts the contents of the Celix ZIP.',
+                     dependsOn: TaskResolver.findTask(project, COPY_CELIX_TASK_NAME)) {
+            // Use the celix zip copied to the build directory.
+            from { project.zipTree("${project.buildDir}/celix.zip") }
+            // Unpack it to the location specified in the extension.
+            into { extension.distributionDir }
+            // We don't need headers to run so exclude them.
+            exclude { element -> element.relativePath.segments[0] == 'include' }
+            // Only include the configured bundles.
+            exclude { element ->
+                // Get the name without the file extension.
+                String name = FilenameUtils.removeExtension(element.name)
+                return !element.isDirectory() &&
+                    element.relativePath.segments[0] == 'bundles' &&
+                    !extension.defaultBundlesToInclude.contains(name)
+            }
+        }
     }
 }
