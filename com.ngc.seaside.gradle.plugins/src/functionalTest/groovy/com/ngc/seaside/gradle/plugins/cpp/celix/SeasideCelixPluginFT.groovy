83a4ebd51f86b0f069d4471951e831db96b9ce70
ThomasJHooper <thomas.hooper@ngc.com>
2017-10-24 11:01:55 -0500

    All tests pass.

--- com.ngc.seaside.gradle.plugins/src/functionalTest/groovy/com/ngc/seaside/gradle/plugins/cpp/celix/SeasideCelixPluginFT.groovy
+++ com.ngc.seaside.gradle.plugins/src/functionalTest/groovy/com/ngc/seaside/gradle/plugins/cpp/celix/SeasideCelixPluginFT.groovy
@@ -54,16 +54,29 @@ class SeasideCelixPluginFT {
               "build",
               "distributions",
               "com.ngc.blocs.cpp.service.event.impl.synceventservice-1.0-SNAPSHOT",
-              "META-INF"))
+              "META-INF",
+              "MANIFEST.MF"))
         assertTrue("manifest file not created!",
                    manifestFile.toFile().exists())
 
         manifestFile.toFile().withInputStream { stream ->
             Manifest manifest = new Manifest()
             manifest.read(stream)
-            assertEquals("Bundle-SymbolicName not correct",
+            assertEquals("Bundle-SymbolicName not correct!",
                          "com.ngc.blocs.cpp.service.event.impl.synceventservice",
                          manifest.getMainAttributes().getValue("Bundle-SymbolicName"))
+            assertEquals("Bundle-Version not correct!",
+                         "1.0.0.SNAPSHOT",
+                         manifest.getMainAttributes().getValue("Bundle-Version"))
+            assertEquals("Bundle-Name not correct!",
+                         "com.ngc.blocs.cpp.service.event.impl.synceventservice",
+                         manifest.getMainAttributes().getValue("Bundle-Name"))
+            assertEquals("Bundle-Activator not correct!",
+                         "lib/linux_x86_64/libservice.event.impl.synceventservice.so",
+                         manifest.getMainAttributes().getValue("Bundle-Activator"))
+            assertEquals("Private-Library not correct!",
+                         "lib/linux_x86_64/libservice.event.impl.synceventservice.so",
+                         manifest.getMainAttributes().getValue("Private-Library"))
         }
     }
 }
