92dd9de38a4f25ba55337772a29d9a3854f3e5ce
ThomasJHooper <thomas.hooper@ngc.com>
2017-10-24 12:01:55 -0500

    Trying to get disto plugin for celix to work.

--- com.ngc.seaside.gradle.plugins/src/main/groovy/com/ngc/seaside/gradle/plugins/cpp/celix/CelixDistributionPlugin.groovy
+++ com.ngc.seaside.gradle.plugins/src/main/groovy/com/ngc/seaside/gradle/plugins/cpp/celix/CelixDistributionPlugin.groovy
@@ -3,10 +3,12 @@ package com.ngc.seaside.gradle.plugins.cpp.celix
 import com.ngc.seaside.gradle.extensions.cpp.celix.CelixDistributionExtension
 import com.ngc.seaside.gradle.plugins.util.GradleUtil
 import com.ngc.seaside.gradle.plugins.util.TaskResolver
+import com.ngc.seaside.gradle.tasks.cpp.celix.CreateCelixRunScriptTask
 import org.apache.commons.io.FilenameUtils
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.bundling.Zip
 
 class CelixDistributionPlugin implements Plugin<Project> {
 
@@ -21,6 +23,9 @@ class CelixDistributionPlugin implements Plugin<Project> {
     public static final String DISTRIBUTION_TASK_GROUP_NAME = "Celix Distribution"
 
     public static final String EXTENSION_NAME = "celixDistribution"
+
+    public static final String CELIX_CONFIGURAION_NAME = "celix"
+    public static final String BUNDLES_CONFIGURATION_NAME = "bundles"
 
     private CelixDistributionExtension extension
 
@@ -48,6 +53,13 @@ class CelixDistributionPlugin implements Plugin<Project> {
                         url nexusConsolidated
                     }
                 }
+
+                postEvaluateConfigureTasks(p)
+                configureTaskDependencies(p)
+
+//                artifacts {
+//                    archives TaskResolver.findTask(p, CREATE_DISTRIBUTION_ZIP_TASK_NAME)
+//                }
             }
         }
     }
@@ -77,7 +89,7 @@ class CelixDistributionPlugin implements Plugin<Project> {
                      group: DISTRIBUTION_TASK_GROUP_NAME,
                      type: Copy,
                      description: 'Copies the Celix ZIP artifact to the build directory.') {
-            from project.configurations.getByName("celix")
+            from project.configurations.getByName(CELIX_CONFIGURAION_NAME)
             into { "${project.buildDir}" }
             rename { name -> "celix.zip" }
         }
@@ -85,8 +97,7 @@ class CelixDistributionPlugin implements Plugin<Project> {
         project.task(UNPACK_CELIX_TASK_NAME,
                      group: DISTRIBUTION_TASK_GROUP_NAME,
                      type: Copy,
-                     description: 'Extracts the contents of the Celix ZIP.',
-                     dependsOn: TaskResolver.findTask(project, COPY_CELIX_TASK_NAME)) {
+                     description: 'Extracts the contents of the Celix ZIP.') {
             // Use the celix zip copied to the build directory.
             from { project.zipTree("${project.buildDir}/celix.zip") }
             // Unpack it to the location specified in the extension.
@@ -98,9 +109,51 @@ class CelixDistributionPlugin implements Plugin<Project> {
                 // Get the name without the file extension.
                 String name = FilenameUtils.removeExtension(element.name)
                 return !element.isDirectory() &&
-                    element.relativePath.segments[0] == 'bundles' &&
-                    !extension.defaultBundlesToInclude.contains(name)
+                       element.relativePath.segments[0] == 'bundles' &&
+                       !extension.defaultBundlesToInclude.contains(name)
             }
         }
+
+        project.task(COPY_BUNDLES_TASK_NAME,
+                     group: DISTRIBUTION_TASK_GROUP_NAME,
+                     type: Copy,
+                     description: 'Copies bundles to the distribution.') {
+            from project.configurations.getByName(BUNDLES_CONFIGURATION_NAME)
+            into { "${extension.distributionDir}/bundles" }
+        }
+
+        project.task(CREATE_RUN_SCRIPT_TASK_NAME,
+                     group: DISTRIBUTION_TASK_GROUP_NAME,
+                     type: CreateCelixRunScriptTask,
+                     description: 'Creates a run script to run Celix.')
+
+        project.task(CREATE_DISTRIBUTION_ZIP_TASK_NAME,
+                     group: DISTRIBUTION_TASK_GROUP_NAME,
+                     type: Zip,
+                     description: 'Creates a ZIP of the distribution.') {
+            from { extension.distributionDir }
+        }
+    }
+
+    protected void postEvaluateConfigureTasks(Project project) {
+        TaskResolver.findTask(project, CREATE_RUN_SCRIPT_TASK_NAME, {
+            scriptFile = extension.runScript
+        })
+    }
+
+    protected void configureTaskDependencies(Project project) {
+        def distroTask = TaskResolver.findTask(project, CREATE_DISTRIBUTION_ZIP_TASK_NAME)
+
+        TaskResolver.findTask(project, UNPACK_CELIX_TASK_NAME).dependsOn(
+              TaskResolver.findTask(project, COPY_CELIX_TASK_NAME))
+
+        TaskResolver.findTask(project, CREATE_RUN_SCRIPT_TASK_NAME).dependsOn(
+              TaskResolver.findTask(project, UNPACK_CELIX_TASK_NAME))
+
+//        distroTask.dependsOn(TaskResolver.findTask(project, CREATE_RUN_SCRIPT_TASK_NAME),
+//                             TaskResolver.findTask(project, COPY_BUNDLES_TASK_NAME))
+
+        TaskResolver.findTask(project, BUILD_TASK_NAME).dependsOn(distroTask)
+        TaskResolver.findTask(project, ASSEMBLE_TASK_NAME).dependsOn(distroTask)
     }
 }
