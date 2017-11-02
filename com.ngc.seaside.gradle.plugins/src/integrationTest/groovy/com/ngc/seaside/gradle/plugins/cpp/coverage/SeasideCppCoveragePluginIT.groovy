b59af4e962cc2183ba0d9c7767ffe74218e91318
Valentine Nwachukwu <valentine.nwachukwu@ngc.com>
2017-11-02 14:30:09 -0500

    fix IT

--- com.ngc.seaside.gradle.plugins/src/integrationTest/groovy/com/ngc/seaside/gradle/plugins/cpp/coverage/SeasideCppCoveragePluginIT.groovy
+++ com.ngc.seaside.gradle.plugins/src/integrationTest/groovy/com/ngc/seaside/gradle/plugins/cpp/coverage/SeasideCppCoveragePluginIT.groovy
@@ -12,56 +12,52 @@ import java.nio.file.Files
 import java.nio.file.Paths
 
 class SeasideCppCoveragePluginIT {
-   private SeasideCppCoveragePlugin plugin
-   private Project project
 
-   @Before
-   void before() {
-      def testProjectDir = createTheTestProjectDirectory()
-      copyTheTestProjectInto(testProjectDir)
-      project = createTheTestProjectUsing(testProjectDir)
-      plugin = new SeasideCppCoveragePlugin()
-      plugin.apply(project)
-   }
+    private SeasideCppCoveragePlugin plugin
+    private Project project
 
-   @Test
-   void appliesPlugin() {
-      Assert.assertNotNull(project.extensions.findByName(SeasideCppCoveragePlugin.CPP_COVERAGE_EXTENSION_NAME))
-      Assert.assertNotNull(TaskResolver.findTask(project, SeasideCppCoveragePlugin.EXTRACT_COVERAGE_TOOLS_TASK_NAME))
-      Assert.assertNotNull(TaskResolver.findTask(project, SeasideCppCoveragePlugin.GENERATE_COVERAGE_DATA_TASK_NAME))
-      Assert.assertNotNull(TaskResolver.findTask(project, SeasideCppCoveragePlugin.GENERATE_CPPCHECK_REPORT_TASK_NAME))
-      Assert.assertNotNull(TaskResolver.findTask(project, SeasideCppCoveragePlugin.GENERATE_RATS_REPORT_TASK_NAME))
-      Assert.assertNotNull(TaskResolver.findTask(project, SeasideCppCoveragePlugin.GENERATE_FULL_COVERAGE_REPORT_TASK_NAME))
-   }
+    @Before
+    void before() {
+        def testProjectDir = createTheTestProjectDirectory()
+        copyTheTestProjectInto(testProjectDir)
+        project = createTheTestProjectUsing(testProjectDir)
+        plugin = new SeasideCppCoveragePlugin()
+        plugin.apply(project)
+    }
 
-   private static File createTheTestProjectDirectory() {
-      def dir = pathToTheDestinationProjectDirectory()
-      return Files.createDirectories(dir.toPath()).toFile()
-   }
+    @Test
+    void appliesPlugin() {
+        Assert.assertNotNull(project.extensions.findByName(SeasideCppCoveragePlugin.CPP_COVERAGE_EXTENSION_NAME))
+        Assert.assertNotNull(TaskResolver.findTask(project, SeasideCppCoveragePlugin.GENERATE_COVERAGE_DATA_TASK_NAME))
+        Assert.assertNotNull(
+                TaskResolver.findTask(project, SeasideCppCoveragePlugin.GENERATE_CPPCHECK_REPORT_TASK_NAME))
+        Assert.assertNotNull(TaskResolver.findTask(project, SeasideCppCoveragePlugin.GENERATE_RATS_REPORT_TASK_NAME))
+        Assert.assertNotNull(
+                TaskResolver.findTask(project, SeasideCppCoveragePlugin.GENERATE_FULL_COVERAGE_REPORT_TASK_NAME))
+    }
 
-   private static File pathToTheDestinationProjectDirectory() {
-      return turnListIntoPath(
-            "build", "integrationTest",
-            "cpp", "coverage", "sealion-java-hello-world"
-      )
-   }
+    private static File createTheTestProjectDirectory() {
+        def dir = pathToTheDestinationProjectDirectory()
+        return Files.createDirectories(dir.toPath()).toFile()
+    }
 
-   private static void copyTheTestProjectInto(File dir) {
-      FileUtils.copyDirectory(sourceDirectoryWithTheTestProject(), dir)
-   }
+    private static File pathToTheDestinationProjectDirectory() {
+        return turnListIntoPath("build", "integrationTest", "cpp", "coverage", "sealion-java-hello-world")
+    }
 
-   private static File sourceDirectoryWithTheTestProject() {
-      return turnListIntoPath(
-            "src", "integrationTest", "resources",
-            "sealion-java-hello-world"
-      )
-   }
+    private static void copyTheTestProjectInto(File dir) {
+        FileUtils.copyDirectory(sourceDirectoryWithTheTestProject(), dir)
+    }
 
-   private static Project createTheTestProjectUsing(File dir) {
-      return ProjectBuilder.builder().withProjectDir(dir).build()
-   }
+    private static File sourceDirectoryWithTheTestProject() {
+        return turnListIntoPath("src", "integrationTest", "resources", "sealion-java-hello-world")
+    }
 
-   private static File turnListIntoPath(String... list) {
-      return Paths.get(list.flatten().join(File.separator)).toFile()
-   }
+    private static Project createTheTestProjectUsing(File dir) {
+        return ProjectBuilder.builder().withProjectDir(dir).build()
+    }
+
+    private static File turnListIntoPath(String... list) {
+        return Paths.get(list.flatten().join(File.separator)).toFile()
+    }
 }
