da1155c242b0558e9708c1fea4c9e7297fc50485
Thomas Hooper <thomas.hooper@ngc.com>
2018-01-16 12:20:07 -0600

    Add FT for maven m2 repo.

---
+++ com.ngc.seaside.gradle.plugins/src/functionalTest/groovy/com/ngc/seaside/gradle/tasks/dependencies/PopulateMaven2RepositoryFT.groovy
@@ -0,0 +1,45 @@
+package com.ngc.seaside.gradle.tasks.dependencies
+
+import com.ngc.seaside.gradle.util.test.TestingUtilities
+import org.apache.commons.io.FileUtils
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
+class PopulateMaven2RepositoryFT {
+    private File projectDir
+    private Project project
+    private List<File> pluginClasspath
+
+    @Before
+    void before() {
+        pluginClasspath = TestingUtilities.getTestClassPath(getClass())
+
+        File source = Paths.get("src/functionalTest/resources/distribution/com.ngc.example.m2repo").toFile()
+        Path targetPath = Paths.get("build/functionalTest/m2repo/com.ngc.example.m2repo")
+        projectDir = Files.createDirectories(targetPath).toFile()
+        FileUtils.copyDirectory(source, projectDir)
+
+        project = ProjectBuilder.builder().withProjectDir(projectDir).build()
+    }
+
+    @Test
+    void doesPopulateM2Repo() {
+        BuildResult result = GradleRunner.create().withProjectDir(projectDir)
+              .withPluginClasspath(pluginClasspath)
+              .forwardOutput()
+              .withArguments("clean", "m2repo")
+              .build()
+
+        Assert.assertEquals(TaskOutcome.valueOf("SUCCESS"), result.task(":m2repo").getOutcome())
+    }
+}
--- com.ngc.seaside.gradle.plugins/src/main/java/com/ngc/seaside/gradle/tasks/dependencies/PopulateMaven2Repository.java
+++ com.ngc.seaside.gradle.plugins/src/main/java/com/ngc/seaside/gradle/tasks/dependencies/PopulateMaven2Repository.java
@@ -1,7 +1,12 @@
 package com.ngc.seaside.gradle.tasks.dependencies;
 
 import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
 
 public class PopulateMaven2Repository extends DefaultTask {
 
+   @TaskAction
+   public void populateRepository() {
+
+   }
 }
