86ea529541163534675c4f588fe3a45ae44ee39f
ThomasJHooper <thomas.hooper@ngc.com>
2018-01-17 14:15:20 -0600

    Update logging.

--- com.ngc.seaside.gradle.plugins/src/main/java/com/ngc/seaside/gradle/tasks/dependencies/PopulateMaven2Repository.java
+++ com.ngc.seaside.gradle.plugins/src/main/java/com/ngc/seaside/gradle/tasks/dependencies/PopulateMaven2Repository.java
@@ -8,13 +8,23 @@ import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
 import org.eclipse.aether.DefaultRepositorySystemSession;
 import org.eclipse.aether.RepositorySystem;
 import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.collection.CollectRequest;
 import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory;
 import org.eclipse.aether.impl.DefaultServiceLocator;
+import org.eclipse.aether.repository.LocalRepository;
 import org.eclipse.aether.repository.RemoteRepository;
+import org.eclipse.aether.resolution.ArtifactResolutionException;
+import org.eclipse.aether.resolution.ArtifactResult;
+import org.eclipse.aether.resolution.DependencyRequest;
+import org.eclipse.aether.resolution.DependencyResolutionException;
+import org.eclipse.aether.resolution.DependencyResult;
 import org.eclipse.aether.spi.connector.RepositoryConnectorFactory;
 import org.eclipse.aether.spi.connector.transport.TransporterFactory;
 import org.eclipse.aether.transport.file.FileTransporterFactory;
 import org.eclipse.aether.transport.http.HttpTransporterFactory;
+import org.eclipse.aether.util.repository.AuthenticationBuilder;
 import org.gradle.api.Action;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.artifacts.Configuration;
@@ -178,31 +188,31 @@ public class PopulateMaven2Repository extends DefaultTask {
 
    protected RepositorySystemSession newSession(RepositorySystem repositorySystem) {
       DefaultRepositorySystemSession session = MavenRepositorySystemUtils.newSession();
-//      File localMavenRepo = repositoryConfiguration.getLocalMavenRepository().toFile();
-////      Preconditions.checkState(!localMavenRepo.isFile() || localMavenRepo.isDirectory(),
-////                               "%s is a file, cannot create directory");
-//      // TODO TH: what if repo does not exists?
-//      if (localMavenRepo.isDirectory()) {
-//         getLogger().debug("Using local maven repository {}.", localMavenRepo);
-//         session.setLocalRepositoryManager(repositorySystem.newLocalRepositoryManager(
-//               session,
-//               new LocalRepository(localMavenRepo)));
-//      }
-//
 
-      if (remoteRepository == null) {
+      File localMavenRepo = Paths.get(localRepository.getUrl()).toFile();
+      session.setLocalRepositoryManager(repositorySystem.newLocalRepositoryManager(
+            session,
+            new LocalRepository(localMavenRepo)));
+
+      if (remoteRepository != null) {
+         RemoteRepository.Builder repoBuilder = new RemoteRepository.Builder(
+               remoteRepository.getName(),
+               "default",
+               remoteRepository.getUrl().toString());
+
+         if (remoteRepository.getCredentials() != null
+             && remoteRepository.getCredentials().getUsername() != null
+             && remoteRepository.getCredentials().getPassword() != null) {
+            repoBuilder.setAuthentication(new AuthenticationBuilder()
+                                                .addUsername(remoteRepository.getCredentials().getUsername())
+                                                .addPassword(remoteRepository.getCredentials().getPassword())
+                                                .build());
+         }
+
+         remoteRepositories.add(repoBuilder.build());
+      } else {
          getLogger().lifecycle("No remote repository configured, downloads will not be attempted.");
       }
-
-//      // TODO TH: what if no remote repository is configured.
-//      remoteRepositories.add(
-//            new RemoteRepository.Builder("remote", "default", repositoryConfiguration.getRemoteRepositoryUrl())
-////                  .setAuthentication(new AuthenticationBuilder()
-////                                           .addUsername(repositoryConfiguration.getRemoteRepositoryUsername())
-////                                           .addPassword(repositoryConfiguration.getRemoteRepositoryPassword())
-////                                           .build())
-//                  .build()
-//      );
 
       return session;
    }
@@ -268,39 +278,59 @@ public class PopulateMaven2Repository extends DefaultTask {
                                String version,
                                String classifier,
                                String extension) {
-      String logMsg = String.format("%s:%s:%s%s@%s",
-                                    groupId,
-                                    artifactId,
-                                    version,
-                                    classifier == null ? "" : ":" + classifier,
-                                    extension);
+      String prettyGave = String.format("%s:%s:%s%s@%s",
+                                        groupId,
+                                        artifactId,
+                                        version,
+                                        classifier == null ? "" : ":" + classifier,
+                                        extension);
       String remoteLogMsg = remoteRepository == null ? "no remote repository configured, no download possible"
                                                      : "download may be required";
-      getLogger().lifecycle("Retrieving '{}' and its dependencies ({}) ...", logMsg, remoteLogMsg);
+      getLogger().lifecycle("Retrieving '{}' and its dependencies ({}) ...", prettyGave, remoteLogMsg);
 
-//      CollectRequest request = new CollectRequest();
-//      Artifact baseArtifact = classifier == null
-//                              ? new DefaultArtifact(groupId, artifactId, extension, version)
-//                              : new DefaultArtifact(groupId, artifactId, classifier, extension, version);
-//      request.setRoot(new org.eclipse.aether.graph.Dependency(baseArtifact, null));
-//      request.setRepositories(remoteRepositories);
-//
-//      DependencyRequest dependencyRequest = new DependencyRequest(request, null);
-//      try {
-//         DependencyResult result = repositorySystem.resolveDependencies(session, dependencyRequest);
-//         for(ArtifactResult localArtifact : result.getArtifactResults()) {
-//            getLogger().lifecycle("Located {}.", localArtifact.getArtifact().getFile());
-//         }
-//      } catch (DependencyResolutionException e) {
-//         if(e.getCause() instanceof ArtifactResolutionException) {
-//            getLogger().lifecycle("Did not find artifact '" + logMsg + "'.");
-//            //getLogger().debug("Exception was:", e);
-//         } else {
-//            getLogger().error("Error while resolving '" + logMsg + "'.", e);
-//         }
-//      }
+      CollectRequest request = new CollectRequest();
+      Artifact baseArtifact = classifier == null
+                              ? new DefaultArtifact(groupId, artifactId, extension, version)
+                              : new DefaultArtifact(groupId, artifactId, classifier, extension, version);
+      request.setRoot(new org.eclipse.aether.graph.Dependency(baseArtifact, null));
+      request.setRepositories(remoteRepositories);
+
+      DependencyRequest dependencyRequest = new DependencyRequest(request, null);
+      try {
+         DependencyResult result = repositorySystem.resolveDependencies(session, dependencyRequest);
+         for (ArtifactResult localArtifact : result.getArtifactResults()) {
+            getLogger().lifecycle("Located {}.", localArtifact.getArtifact().getFile());
+         }
+      } catch (DependencyResolutionException e) {
+         handleResolutionException(e,
+                                   groupId,
+                                   artifactId,
+                                   version,
+                                   classifier,
+                                   extension,
+                                   prettyGave);
+      }
 
       return null;
    }
 
+   private void handleResolutionException(DependencyResolutionException e,
+                                          String groupId,
+                                          String artifactId,
+                                          String version,
+                                          String classifier,
+                                          String extension,
+                                          String prettyGave) {
+      if (e.getCause() instanceof ArtifactResolutionException) {
+         if (DEFAULT_CLASSIFIERS.contains(classifier)) {
+            getLogger().lifecycle("Did not resolve '{}' but that is okay since that artifact is only {}.",
+                                  prettyGave,
+                                  classifier);
+         } else {
+            getLogger().warn("Failed to resolve '{}' (this artifact may be required by the build).", prettyGave);
+         }
+      } else {
+         getLogger().error("Encountered unexpected error while resolving '{}'.", prettyGave, e);
+      }
+   }
 }
