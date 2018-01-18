04efc2f0086979f4a5c0191eac6bcb5a5ca59f8c
ThomasJHooper <thomas.hooper@ngc.com>
2018-01-18 10:55:21 -0600

    Implement IT.

--- com.ngc.seaside.gradle.plugins/src/test/groovy/com/ngc/seaside/gradle/util/test/GradleMocks.groovy
+++ com.ngc.seaside.gradle.plugins/src/test/groovy/com/ngc/seaside/gradle/util/test/GradleMocks.groovy
@@ -1,6 +1,12 @@
 package com.ngc.seaside.gradle.util.test
 
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.artifacts.ConfigurationContainer
+import org.gradle.api.artifacts.Dependency
+import org.gradle.api.artifacts.DependencySet
+import org.gradle.api.internal.DefaultDomainObjectSet
 import org.gradle.api.internal.project.ProjectInternal
+import org.gradle.api.tasks.TaskDependency
 import org.gradle.internal.service.ServiceRegistry
 import org.gradle.util.Path
 
@@ -16,7 +22,7 @@ class GradleMocks {
     }
 
     /**
-     * Creates a new Gradle {@code Project} that contains a mocked service registry.
+     * Creates a new Gradle {@code Project} that contains a mocked service registry and configuration container.
      */
     static ProjectInternal newProjectMock() {
         // We have to reference the internal Gradle API ProjectInternal because that is what is needed to
@@ -30,6 +36,27 @@ class GradleMocks {
         when(project.getProjectPath()).thenReturn(projectPath)
         when(project.getIdentityPath()).thenReturn(projectPath)
 
+        ConfigurationContainer configs = mock(ConfigurationContainer)
+        when(project.getConfigurations()).thenReturn(configs)
+
         return project
+    }
+
+    static Configuration newConfiguration(String name) {
+        Configuration config = mock(Configuration)
+        when(config.getName()).thenReturn(name)
+        when(config.getDependencies()).thenReturn(new MockedDependencySet())
+        return config
+    }
+
+    private static class MockedDependencySet extends DefaultDomainObjectSet implements DependencySet {
+        private MockedDependencySet() {
+            super(Dependency)
+        }
+
+        @Override
+        TaskDependency getBuildDependencies() {
+            throw new UnsupportedOperationException("not implemented")
+        }
     }
 }
