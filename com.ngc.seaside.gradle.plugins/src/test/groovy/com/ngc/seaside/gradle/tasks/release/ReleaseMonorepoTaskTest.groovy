88e319863b4e3ac249f5a6eb05206c141d71c507
Cameron Chaparro <cameron.chaparro@ngc.com>
2018-01-22 17:08:05 -0600

    Add task shell for releasing a monorepo
    Add a task that will slightly extend the ReleaseTask to ensure that no release
    will occur if any subproject fails to be release. This will need a better name
    but, as of right now, I can't think of one...

---
+++ com.ngc.seaside.gradle.plugins/src/test/groovy/com/ngc/seaside/gradle/tasks/release/ReleaseMonorepoTaskTest.groovy
@@ -0,0 +1,18 @@
+package com.ngc.seaside.gradle.tasks.release
+
+import com.ngc.seaside.gradle.tasks.release.ReleaseMonorepoTask
+
+import org.junit.Assert;
+import org.junit.Test;
+
+class ReleaseMonorepoTaskTest {
+    @Test
+    void doesReleaseWhenAllSubprojectsAreSuccessfullyReleased() {
+        Assert.fail("implement me!")
+    }
+
+    @Test
+    void doesNotReleaseWhenAnySubprojectFailsToBeReleased() {
+        Assert.fail("implement me!")
+    }
+}
