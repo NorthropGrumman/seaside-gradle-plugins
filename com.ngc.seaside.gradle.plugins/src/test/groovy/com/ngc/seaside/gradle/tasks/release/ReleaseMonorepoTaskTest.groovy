f19914f9632c9cc665f2edf1efb497ae44ae2186
Cameron Chaparro <cameron.chaparro@ngc.com>
2018-01-22 18:31:24 -0600

    Add a mocked project
    We will need to test that the task is doing the right thing but, from what I
    recall and from what I'm seeing online, Gradle doesn't really allow you to
    create tasks except through the DSL. Thus, we'll need to figure out how to unit
    test this or, if necessary, forget about unit-testing the task itself and just
    unit-test helper functions. This would, then, require that we do integration and
    functional tests for this, instead.

--- com.ngc.seaside.gradle.plugins/src/test/groovy/com/ngc/seaside/gradle/tasks/release/ReleaseMonorepoTaskTest.groovy
+++ com.ngc.seaside.gradle.plugins/src/test/groovy/com/ngc/seaside/gradle/tasks/release/ReleaseMonorepoTaskTest.groovy
@@ -1,11 +1,23 @@
 package com.ngc.seaside.gradle.tasks.release
 
 import com.ngc.seaside.gradle.tasks.release.ReleaseMonorepoTask
+import org.gradle.api.Project
+import java.io.File
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.Assert
+import org.junit.Test
+
+import org.mockito.Mockito
 
 class ReleaseMonorepoTaskTest {
+    private Project project = Mockito.mock(Project)
+    private File file = Mockito.mock(File)
+
+    @Before
+    void before() {
+        Mockito.when(project.buildDir).thenReturn(file)
+    }
+
     @Test
     void doesReleaseWhenAllSubprojectsAreSuccessfullyReleased() {
         Assert.fail("implement me!")
