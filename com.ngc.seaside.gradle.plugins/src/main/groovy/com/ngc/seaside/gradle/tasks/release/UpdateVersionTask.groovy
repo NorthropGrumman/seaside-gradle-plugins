b8a7b8b476de02b70fe477b5699e7f6f41b36caa
Cameron Chaparro <cameron.chaparro@ngc.com>
2018-01-25 18:14:34 -0600

    Add more unit tests
    Add some more unit tests for the UpdateVersionTask to make it a bit more
    complete. It's worth noting that the PATCH version doesn't seem to be
    updating correctly, but the others release types seem to be working.

--- com.ngc.seaside.gradle.plugins/src/main/groovy/com/ngc/seaside/gradle/tasks/release/UpdateVersionTask.groovy
+++ com.ngc.seaside.gradle.plugins/src/main/groovy/com/ngc/seaside/gradle/tasks/release/UpdateVersionTask.groovy
@@ -1,16 +1,45 @@
 package com.ngc.seaside.gradle.tasks.release
 
+import com.google.common.base.Preconditions
+import com.ngc.seaside.gradle.util.VersionResolver
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
 class UpdateVersionTask extends DefaultTask {
+    private final VersionResolver resolver
     private ReleaseType typeOfRelease
 
-    @TaskAction
-    def updateReleaseVersion() {}
+    UpdateVersionTask() {
+        this.resolver = new VersionResolver(project)
+        this.typeOfRelease = ReleaseType.PATCH
+    }
 
-    void prepareForSnapshotRelease() {
-        typeOfRelease = ReleaseType.SNAPSHOT
+    UpdateVersionTask(VersionResolver resolver, ReleaseType typeOfRelease = ReleaseType.PATCH) {
+        this.resolver = Preconditions.checkNotNull(resolver, "resolver may not be null!")
+        this.typeOfRelease = typeOfRelease
+    }
+
+    @TaskAction
+    def updateReleaseVersion() {
+        Preconditions.checkState(
+              isReleaseVersionSet(),
+              "Must call prepareForReleaseIfNeeded() during configuration phase."
+        )
+        def newReleaseVersion = getVersionForRelease()
+        println("the new release version will be: $newReleaseVersion")
+    }
+
+    boolean isReleaseVersionSet() {
+        return project.rootProject.hasProperty("releaseVersion")
+    }
+
+    String getVersionForRelease() {
+        def upgradeStrategy = resolver.resolveVersionUpgradeStrategy(releaseType)
+        return upgradeStrategy.getVersion(getCurrentVersion())
+    }
+
+    String getCurrentVersion() {
+        return resolver.getProjectVersion(releaseType)
     }
 
     ReleaseType getReleaseType() {
--- com.ngc.seaside.gradle.plugins/src/test/groovy/com/ngc/seaside/gradle/tasks/release/UpdateVersionTaskTest.groovy
+++ com.ngc.seaside.gradle.plugins/src/test/groovy/com/ngc/seaside/gradle/tasks/release/UpdateVersionTaskTest.groovy
@@ -1,28 +1,92 @@
 package com.ngc.seaside.gradle.tasks.release
 
 import com.ngc.seaside.gradle.plugins.release.SeasideReleaseMonoRepoPlugin
+import com.ngc.seaside.gradle.util.VersionResolver
 import com.ngc.seaside.gradle.util.test.GradleMocks
 import com.ngc.seaside.gradle.util.test.TaskBuilder
+import org.gradle.api.internal.project.ProjectInternal
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
 
+import static org.mockito.Mockito.when
+import static org.mockito.Mockito.verify
+
+@RunWith(MockitoJUnitRunner.Silent)
 class UpdateVersionTaskTest {
-    private static final String TEST_RELEASE_VERSION = "1.2.3"
+    private static final String TEST_VERSION_SUFFIX = "-SNAPSHOT"
+    private static final String TEST_UPGRADE_VERSION = "1.2.2"
+    private static final String TEST_RELEASE_SNAPSHOT_VERSION = "${TEST_UPGRADE_VERSION}${TEST_VERSION_SUFFIX}"
+    private static final String TEST_RELEASE_PATCH_VERSION = "1.2.3"
+    private static final String TEST_RELEASE_MINOR_VERSION = "1.3.0"
+    private static final String TEST_RELEASE_MAJOR_VERSION = "2.0.0"
 
+    private ProjectInternal testProject
     private UpdateVersionTask task
+
+    @Mock
+    private VersionResolver resolver
+
+    @Mock
+    private IVersionUpgradeStrategy upgradeStrategy
 
     @Before
     void before() {
-        task = new TaskBuilder<UpdateVersionTask>(UpdateVersionTask)
-            .setProject(GradleMocks.newProjectMock())
-            .setName(SeasideReleaseMonoRepoPlugin.RELEASE_UPDATE_VERSION_TASK_NAME)
-            .create()
+        testProject = GradleMocks.newProjectMock()
+        when(testProject.rootProject).thenReturn(testProject)
+        when(testProject.rootProject.hasProperty("releaseVersion")).thenReturn(true)
+
+        // TODO(Cameron): Figure out how to make this a single line
+        // i.e. I want to be able to pass any argument and it will always return the same TEST_UPGRADE_VERSION string
+        when(resolver.getProjectVersion(ReleaseType.SNAPSHOT)).thenReturn(TEST_RELEASE_SNAPSHOT_VERSION)
+        when(resolver.getProjectVersion(ReleaseType.PATCH)).thenReturn(TEST_UPGRADE_VERSION)
+        when(resolver.getProjectVersion(ReleaseType.MINOR)).thenReturn(TEST_UPGRADE_VERSION)
+        when(resolver.getProjectVersion(ReleaseType.MAJOR)).thenReturn(TEST_UPGRADE_VERSION)
+    }
+
+    @Test(expected = IllegalStateException.class)
+    void doesFailIfReleaseVersionPropertyIsNotSetOnProject() {
+        when(testProject.rootProject.hasProperty("releaseVersion")).thenReturn(false)
+        task = createTaskWithReleaseType(ReleaseType.PATCH)
+        task.updateReleaseVersion()
     }
 
     @Test
-    void canPerformSnapshotRelease() {
-        task.prepareForSnapshotRelease()
-        Assert.assertEquals(ReleaseType.SNAPSHOT, task.getReleaseType())
+    void doesNotUpgradeVersionForSnapshotRelease() {
+        confirmVersionUpgradeForReleaseType(ReleaseType.SNAPSHOT, TEST_RELEASE_SNAPSHOT_VERSION)
+    }
+
+    @Test
+    void canUpgradeVersionForPatchRelease() {
+        confirmVersionUpgradeForReleaseType(ReleaseType.PATCH, TEST_RELEASE_PATCH_VERSION)
+    }
+
+    @Test
+    void canUpgradeVersionForMinorRelease() {
+        confirmVersionUpgradeForReleaseType(ReleaseType.MINOR, TEST_RELEASE_MINOR_VERSION)
+    }
+
+    @Test
+    void canUpgradeVersionForMajorRelease() {
+        confirmVersionUpgradeForReleaseType(ReleaseType.MAJOR, TEST_RELEASE_MAJOR_VERSION)
+    }
+
+    private void confirmVersionUpgradeForReleaseType(ReleaseType releaseType, String expectedUpgradeVersion) {
+        task = createTaskWithReleaseType(releaseType)
+        task.updateReleaseVersion()
+        verify(resolver).getProjectVersion(releaseType)
+        Assert.assertEquals(releaseType, task.getReleaseType())
+        Assert.assertEquals(expectedUpgradeVersion, task.getVersionForRelease())
+    }
+
+    private UpdateVersionTask createTaskWithReleaseType(ReleaseType releaseType) {
+        return new TaskBuilder<UpdateVersionTask>(UpdateVersionTask)
+              .setProject(testProject)
+              .setName(SeasideReleaseMonoRepoPlugin.RELEASE_UPDATE_VERSION_TASK_NAME)
+              .setSupplier({ new UpdateVersionTask(resolver, releaseType) })
+              .create()
     }
 }
