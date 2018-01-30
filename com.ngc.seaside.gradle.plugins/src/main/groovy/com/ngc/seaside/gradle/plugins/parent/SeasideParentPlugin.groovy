11a39a745664e98d9dd55bdfdab7deae19e86509
ThomasJHooper <thomas.hooper@ngc.com>
2018-01-30 08:07:05 -0600

    The parent plugin now applies the CI plugin to the root project.

--- com.ngc.seaside.gradle.plugins/src/main/groovy/com/ngc/seaside/gradle/plugins/parent/SeasideParentPlugin.groovy
+++ com.ngc.seaside.gradle.plugins/src/main/groovy/com/ngc/seaside/gradle/plugins/parent/SeasideParentPlugin.groovy
@@ -251,6 +251,10 @@ class SeasideParentPlugin extends AbstractProjectPlugin {
         project.getPlugins().apply('com.github.ksoichiro.console.reporter')
         project.getPlugins().apply(SeasideReleasePlugin)
         project.getPlugins().apply(SeasideCiPlugin)
+
+        // Apply the CI plugin to the root project.  This allows us to run the populateM2repo task and CI related
+        // tasks for root projects as well as subprojects.
+        project.getRootProject().getPlugins().apply(SeasideCiPlugin)
     }
 
     /**
