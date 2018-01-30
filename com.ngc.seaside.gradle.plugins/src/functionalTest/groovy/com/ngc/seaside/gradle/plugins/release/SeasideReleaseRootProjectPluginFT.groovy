303da8cef277f5f6c4eced0161e6bc05333099c0
Cameron Chaparro <cameron.chaparro@ngc.com>
2018-01-30 14:36:37 -0600

    Add ability to make a mono repo test project
    Add a simple test mono repo project.
    
    Update the functional tests to use the test mono repo project.
    
    NB: The functional tests still fail due to build file error but we're
    a step closer, now.

--- com.ngc.seaside.gradle.plugins/src/functionalTest/groovy/com/ngc/seaside/gradle/plugins/release/SeasideReleaseRootProjectPluginFT.groovy
+++ com.ngc.seaside.gradle.plugins/src/functionalTest/groovy/com/ngc/seaside/gradle/plugins/release/SeasideReleaseRootProjectPluginFT.groovy
@@ -15,12 +15,18 @@ class SeasideReleaseRootProjectPluginFT {
     private Project project
     private List<File> pluginClasspath
     private SeasideReleaseRootProjectPlugin plugin
-    private List<String> subprojectNames = [
-            "service.bonjourlemonde",
-            "service.helloworld",
-            "service.holamundo",
-            "service.konnichiwamojuru"
+    private List<String> projectNames = [
+            "bonjourlemonde",
+            "helloworld",
+            "holamundo"
     ]
+    private Map<String, List<String>> subprojectNames = new HashMap<String, List<String>>()
+
+    SeasideReleaseRootProjectPluginFT() {
+        subprojectNames.put(projectNames[0], ["bonjour", "aurevoir"])
+        subprojectNames.put(projectNames[1], ["hello", "goodbye"])
+        subprojectNames.put(projectNames[2], ["hola", "adios"])
+    }
 
     @Before
     void before() {
@@ -75,13 +81,13 @@ class SeasideReleaseRootProjectPluginFT {
 
     private static File sourceDirectoryWithTheTestProject() {
         return TestingUtilities.turnListIntoPath(
-            "src", "functionalTest", "resources", "sealion-java-hello-world"
+            "src", "functionalTest", "resources", "sealion-java-hello-world-monorepo"
         )
     }
 
     private static File pathToTheDestinationProjectDirectory() {
         return TestingUtilities.turnListIntoPath(
-            "build", "functionalTest", "release", "sealion-java-hello-world"
+            "build", "functionalTest", "release", "sealion-java-hello-world-monorepo"
         )
     }
 
@@ -97,15 +103,17 @@ class SeasideReleaseRootProjectPluginFT {
     }
 
     private void checkForTaskSuccess(String taskName) {
-        BuildResult result = GradleRunner.create()
-                .withProjectDir(projectDir)
-                .withPluginClasspath(pluginClasspath)
-                .forwardOutput()
-                .withArguments("clean", "build", taskName)
-                .build()
+        projectNames.each { projectName ->
+            BuildResult result = GradleRunner.create()
+                    .withProjectDir(TestingUtilities.turnListIntoPath(projectDir.toString(), projectName))
+                    .withPluginClasspath(pluginClasspath)
+                    .forwardOutput()
+                    .withArguments("clean", "build", taskName)
+                    .build()
 
-        subprojectNames.each { subprojectName ->
-            TestingUtilities.assertTaskSuccess(result, subprojectName, taskName)
+            subprojectNames.get(projectName).each { subprojectName ->
+                TestingUtilities.assertTaskSuccess(result, subprojectName, taskName)
+            }
         }
     }
 }
