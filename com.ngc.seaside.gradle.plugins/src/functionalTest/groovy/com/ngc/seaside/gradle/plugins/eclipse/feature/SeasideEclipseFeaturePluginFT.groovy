2a3ac994f2654932fb7b070adcf4926c1c534ec0
Cameron Chaparro <cameron.chaparro@ngc.com>
2018-04-16 16:46:12 -0500

    Add initial functional test

---
+++ com.ngc.seaside.gradle.plugins/src/functionalTest/groovy/com/ngc/seaside/gradle/plugins/eclipse/feature/SeasideEclipseFeaturePluginFT.groovy
@@ -0,0 +1,48 @@
+package com.ngc.seaside.gradle.plugins.eclipse.feature
+
+import com.ngc.seaside.gradle.util.test.TestingUtilities
+import org.gradle.api.Project
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Before
+import org.junit.Test
+
+class SeasideEclipseFeaturePluginFT {
+    private List<File> pluginClasspath
+    private Project project
+    private File projectDir
+
+    @Before
+    void before() {
+        pluginClasspath = TestingUtilities.getTestClassPath(getClass())
+        projectDir = TestingUtilities.setUpTheTestProjectDirectory(
+              sourceDirectoryWithTheTestProject(),
+              pathToTheDestinationProjectDirectory()
+        )
+        project = TestingUtilities.createTheTestProjectWith(projectDir)
+    }
+
+    @Test
+    void runsGradleBuildWithSuccess() {
+        BuildResult result = GradleRunner.create()
+              .withProjectDir(projectDir)
+              .withPluginClasspath(pluginClasspath)
+              .forwardOutput()
+              .withArguments("clean", "build")
+              .build()
+
+        TestingUtilities.assertTaskSuccess(result, "service.helloworld", "build")
+    }
+
+    private static File sourceDirectoryWithTheTestProject() {
+        return TestingUtilities.turnListIntoPath(
+              "src", "functionalTest", "resources", "sealion-java-hello-world"
+        )
+    }
+
+    private static File pathToTheDestinationProjectDirectory() {
+        return TestingUtilities.turnListIntoPath(
+              "build", "functionalTest", "eclipse", "feature", "sealion-java-hello-world"
+        )
+    }
+}
