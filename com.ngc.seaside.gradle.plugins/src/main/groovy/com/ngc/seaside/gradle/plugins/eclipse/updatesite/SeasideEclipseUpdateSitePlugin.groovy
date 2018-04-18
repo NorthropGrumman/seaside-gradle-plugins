072e3d62221906b891702c3edede63cd83fe6205
Cameron Chaparro <cameron.chaparro@ngc.com>
2018-04-17 19:47:46 -0500

    Add some tests and implementation to make them pass

--- com.ngc.seaside.gradle.plugins/src/main/groovy/com/ngc/seaside/gradle/plugins/eclipse/updatesite/SeasideEclipseUpdateSitePlugin.groovy
+++ com.ngc.seaside.gradle.plugins/src/main/groovy/com/ngc/seaside/gradle/plugins/eclipse/updatesite/SeasideEclipseUpdateSitePlugin.groovy
@@ -1,4 +1,13 @@
 package com.ngc.seaside.gradle.plugins.eclipse.updatesite
 
-class SeasideEclipseUpdateSitePlugin {
+import com.ngc.seaside.gradle.api.plugins.AbstractProjectPlugin
+import org.gradle.api.Project
+
+class SeasideEclipseUpdateSitePlugin extends AbstractProjectPlugin {
+    public static final String ECLIPSE_EXTENSION_NAME = "seasideEclipse"
+    public static final String ECLIPSE_TASK_GROUP_NAME = "Eclipse"
+
+    @Override
+    void doApply(Project project) {
+    }
 }
--- com.ngc.seaside.gradle.plugins/src/test/groovy/com/ngc/seaside/gradle/plugins/eclipse/updatesite/SeasideEclipseUpdateSitePluginTest.groovy
+++ com.ngc.seaside.gradle.plugins/src/test/groovy/com/ngc/seaside/gradle/plugins/eclipse/updatesite/SeasideEclipseUpdateSitePluginTest.groovy
@@ -1,4 +1,28 @@
 package com.ngc.seaside.gradle.plugins.eclipse.updatesite
 
+import com.ngc.seaside.gradle.api.plugins.AbstractProjectPlugin
+import org.junit.Assert
+import org.junit.Test
+
 class SeasideEclipseUpdateSitePluginTest {
+    @Test
+    void classExists() {
+        def plugin = new SeasideEclipseUpdateSitePlugin()
+        Assert.assertNotNull("plugin does not exist!", plugin)
+    }
+
+    @Test
+    void extendsAbstractProjectPlugin() {
+        def plugin = new SeasideEclipseUpdateSitePlugin()
+        Assert.assertTrue(
+              "plugin is not an instance of AbstractProjectPlugin",
+              plugin instanceof AbstractProjectPlugin
+        )
+    }
+
+    @Test
+    void hasNeededPublicNames() {
+        Assert.assertNotNull("extension name does not exist!", SeasideEclipseUpdateSitePlugin.ECLIPSE_EXTENSION_NAME)
+        Assert.assertNotNull("task group name does not exist!", SeasideEclipseUpdateSitePlugin.ECLIPSE_TASK_GROUP_NAME)
+    }
 }
