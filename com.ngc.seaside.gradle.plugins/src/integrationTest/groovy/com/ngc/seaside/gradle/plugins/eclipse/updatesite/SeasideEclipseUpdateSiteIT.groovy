0213d7ccc5e39755be61910eca565d346bdf4b0b
Cameron Chaparro <cameron.chaparro@ngc.com>
2018-04-18 16:37:19 -0500

    Add some more tests and initial implementation

--- com.ngc.seaside.gradle.plugins/src/integrationTest/groovy/com/ngc/seaside/gradle/plugins/eclipse/updatesite/SeasideEclipseUpdateSiteIT.groovy
+++ com.ngc.seaside.gradle.plugins/src/integrationTest/groovy/com/ngc/seaside/gradle/plugins/eclipse/updatesite/SeasideEclipseUpdateSiteIT.groovy
@@ -1,4 +1,70 @@
 package com.ngc.seaside.gradle.plugins.eclipse.updatesite
 
+import com.ngc.seaside.gradle.util.TaskResolver
+import com.ngc.seaside.gradle.util.test.TestingUtilities
+import org.gradle.api.Project
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Ignore
+import org.junit.Test
+
 class SeasideEclipseUpdateSiteIT {
+    private SeasideEclipseUpdateSitePlugin plugin
+    private TaskResolver resolver
+    private Project project
+    private File projectDir
+    private List<String> taskNames = [
+          SeasideEclipseUpdateSitePlugin.ECLIPSE_COPY_FEATURES_TASK_NAME,
+          SeasideEclipseUpdateSitePlugin.ECLIPSE_COPY_SD_PLUGINS_TASK_NAME,
+          SeasideEclipseUpdateSitePlugin.ECLIPSE_COPY_ECLIPSE_PLUGINS_TASK_NAME,
+          SeasideEclipseUpdateSitePlugin.ECLIPSE_CREATE_METADATA_TASK_NAME,
+          SeasideEclipseUpdateSitePlugin.ECLIPSE_CREATE_ZIP_TASK_NAME,
+    ]
+
+    @Before
+    void before() {
+        projectDir = TestingUtilities.setUpTheTestProjectDirectory(
+              sourceDirectoryWithTheTestProject(),
+              pathToTheDestinationProjectDirectory()
+        )
+        project = TestingUtilities.createTheTestProjectWith(projectDir)
+        plugin = new SeasideEclipseUpdateSitePlugin()
+        plugin.apply(project)
+        resolver = new TaskResolver(project)
+    }
+
+    @Test
+    void tasksExist() {
+        verifyTasksExistOnThePlugin()
+    }
+
+    @Ignore
+    @Test
+    void extensionExists() {
+        Assert.assertNotNull(
+              "eclipse update extension does not exist!",
+              project.extensions.findByName(SeasideEclipseUpdateSitePlugin.ECLIPSE_UPDATE_SITE_EXTENSION_NAME)
+        )
+    }
+
+    private static File sourceDirectoryWithTheTestProject() {
+        return TestingUtilities.turnListIntoPath(
+              "src", "integrationTest", "resources", "sealion-java-hello-world"
+        )
+    }
+
+    private static File pathToTheDestinationProjectDirectory() {
+        return TestingUtilities.turnListIntoPath(
+              "build", "integrationTest", "eclipse", "updatesite", "sealion-java-hello-world"
+        )
+    }
+
+    private void verifyTasksExistOnThePlugin() {
+        taskNames.each { taskName ->
+            Assert.assertNotNull(
+                  "$taskName task does not exist!",
+                  resolver.findTask(taskName)
+            )
+        }
+    }
 }
--- com.ngc.seaside.gradle.plugins/src/main/groovy/com/ngc/seaside/gradle/plugins/eclipse/updatesite/SeasideEclipseUpdateSitePlugin.groovy
+++ com.ngc.seaside.gradle.plugins/src/main/groovy/com/ngc/seaside/gradle/plugins/eclipse/updatesite/SeasideEclipseUpdateSitePlugin.groovy
@@ -2,6 +2,8 @@ package com.ngc.seaside.gradle.plugins.eclipse.updatesite
 
 import com.ngc.seaside.gradle.api.plugins.AbstractProjectPlugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.bundling.Zip
 
 class SeasideEclipseUpdateSitePlugin extends AbstractProjectPlugin {
     public static final String ECLIPSE_TASK_GROUP_NAME = "Eclipse"
@@ -10,9 +12,52 @@ class SeasideEclipseUpdateSitePlugin extends AbstractProjectPlugin {
     public static final String ECLIPSE_COPY_FEATURES_TASK_NAME = "copyFeatures"
     public static final String ECLIPSE_COPY_SD_PLUGINS_TASK_NAME = "copySdPlugins"
     public static final String ECLIPSE_COPY_ECLIPSE_PLUGINS_TASK_NAME = "copyEclipsePlugins"
+    public static final String ECLIPSE_CREATE_METADATA_TASK_NAME = "createMetadata"
     public static final String ECLIPSE_CREATE_ZIP_TASK_NAME = "createZip"
 
     @Override
     void doApply(Project project) {
+        project.configure(project) {
+            createTasks(project)
+
+            defaultTasks = ["build"]
+        }
+    }
+
+    private static void createTasks(Project project) {
+        project.task(
+              ECLIPSE_COPY_FEATURES_TASK_NAME,
+              type: Copy,
+              group: ECLIPSE_TASK_GROUP_NAME,
+              description: "description of: $ECLIPSE_COPY_FEATURES_TASK_NAME")
+
+        project.task(
+              ECLIPSE_COPY_SD_PLUGINS_TASK_NAME,
+              type: Copy,
+              group: ECLIPSE_TASK_GROUP_NAME,
+              description: "description of: $ECLIPSE_COPY_SD_PLUGINS_TASK_NAME")
+
+        project.task(
+              ECLIPSE_COPY_ECLIPSE_PLUGINS_TASK_NAME,
+              type: Copy,
+              group: ECLIPSE_TASK_GROUP_NAME,
+              description: "description of: $ECLIPSE_COPY_ECLIPSE_PLUGINS_TASK_NAME")
+
+        project.task(
+              ECLIPSE_CREATE_METADATA_TASK_NAME,
+              group: ECLIPSE_TASK_GROUP_NAME,
+              description: "description of: $ECLIPSE_CREATE_METADATA_TASK_NAME")
+
+        def task = project.task(
+              ECLIPSE_CREATE_ZIP_TASK_NAME,
+              type: Zip,
+              group: ECLIPSE_TASK_GROUP_NAME,
+              description: "description of: $ECLIPSE_CREATE_ZIP_TASK_NAME")
+
+        project.task("clean") {
+            project.delete(project.buildDir)
+        }
+
+        project.task("build").dependsOn(task)
     }
 }
