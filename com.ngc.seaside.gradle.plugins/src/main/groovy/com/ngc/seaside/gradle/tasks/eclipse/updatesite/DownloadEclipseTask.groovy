a65cd25ce1381f867b88dd0e8096389d1aefc83a
Cameron Chaparro <cameron.chaparro@ngc.com>
2018-04-23 15:31:19 -0500

    Add DownloadEclipseTask
    Add a task that will download eclipse from a user-specified url to a
    user-specified download location and will only perform the download if the file
    has not already been downloaded to that location.

---
+++ com.ngc.seaside.gradle.plugins/src/main/groovy/com/ngc/seaside/gradle/tasks/eclipse/updatesite/DownloadEclipseTask.groovy
@@ -0,0 +1,25 @@
+package com.ngc.seaside.gradle.tasks.eclipse.updatesite
+
+import com.ngc.seaside.gradle.extensions.eclipse.updatesite.SeasideEclipseUpdateSiteExtension
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+import java.nio.file.Paths
+
+class DownloadEclipseTask extends DefaultTask {
+    private SeasideEclipseUpdateSiteExtension extension
+
+    @TaskAction
+    void downloadEclipse() {
+        extension = project.extensions.getByType(SeasideEclipseUpdateSiteExtension.class)
+        def destFile = project.file(Paths.get(extension.cacheDirectory, "${extension.getEclipseVersion()}.zip"))
+        destFile.getParentFile().mkdirs()
+
+        if (!destFile.exists()) {
+            project.logger.info("Downloading Eclipse SDK from ${extension.getEclipseDownloadUrl()}...")
+            new URL(extension.getEclipseDownloadUrl()).withInputStream { is ->
+                destFile.withOutputStream { it << is }
+            }
+        }
+    }
+}
