4aa37e76cf95a805dc3763744b8a48b588dd0a6f
Cameron Chaparro <cameron.chaparro@ngc.com>
2018-04-24 19:40:16 -0500

    Implement unzipEclipse task

--- com.ngc.seaside.gradle.plugins/src/main/groovy/com/ngc/seaside/gradle/tasks/eclipse/updatesite/UnzipEclipseTask.groovy
+++ com.ngc.seaside.gradle.plugins/src/main/groovy/com/ngc/seaside/gradle/tasks/eclipse/updatesite/UnzipEclipseTask.groovy
@@ -1,4 +1,19 @@
 package com.ngc.seaside.gradle.tasks.eclipse.updatesite
 
-class UnzipEclipseTask {
+import com.ngc.seaside.gradle.extensions.eclipse.updatesite.SeasideEclipseUpdateSiteExtension
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+class UnzipEclipseTask extends DefaultTask {
+    @TaskAction
+    void unzipEclipse() {
+        def extension = project.extensions.getByType(SeasideEclipseUpdateSiteExtension.class)
+
+        if (!project.file(extension.eclipseArchiveName - ".zip").exists()) {
+            project.copy {
+                from project.zipTree(project.file(extension.eclipseArchiveName))
+                into extension.cacheDirectory
+            }
+        }
+    }
 }
