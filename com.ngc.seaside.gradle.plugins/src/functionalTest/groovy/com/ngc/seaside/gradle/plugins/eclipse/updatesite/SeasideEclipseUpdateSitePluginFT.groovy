726ad1cd595fa54b677a92383afe3043ac8810f7
Cameron Chaparro <cameron.chaparro@ngc.com>
2018-04-30 13:21:49 -0500

    Update the FT
    For testing, only use one eclipsePlugin that we can explicitly test for
    existence.
    
    Check for existence of the jars we need to include in our update site.

--- com.ngc.seaside.gradle.plugins/src/functionalTest/groovy/com/ngc/seaside/gradle/plugins/eclipse/updatesite/SeasideEclipseUpdateSitePluginFT.groovy
+++ com.ngc.seaside.gradle.plugins/src/functionalTest/groovy/com/ngc/seaside/gradle/plugins/eclipse/updatesite/SeasideEclipseUpdateSitePluginFT.groovy
@@ -40,14 +40,24 @@ class SeasideEclipseUpdateSitePluginFT {
         String projectName = "com.ngc.seaside.service.heiverden"
         String projectBuildDir = Paths.get(pathToTheDestinationProjectDirectory().absolutePath, projectName, "build")
         String updateSitePath = Paths.get(projectBuildDir, "updatesite")
-        String featuresPath = Paths.get(updateSitePath, "features")
-        String pluginsPath = Paths.get(updateSitePath, "plugins")
+        String featuresPath = Paths.get(updateSitePath, "features", "com.ngc.seaside.service.nihao_1.2.3.SNAPSHOT.jar")
+        String customPluginPath = Paths.get(
+              updateSitePath, "plugins", "com.ngc.seaside.service.helloworld_1.2.3.SNAPSHOT.jar")
+        String eclipsePluginPath = Paths.get(updateSitePath, "plugins", "org.antlr.runtime_3.2.0.v201101311130.jar")
         String updateSiteArchivePath = Paths.get(projectBuildDir, "com.ngc.seaside.test.test-name-1.0.0.zip")
 
         Assert.assertTrue("$featuresPath directory was not created!", project.file(featuresPath).exists())
-        Assert.assertTrue("$pluginsPath directory was not created!", project.file(pluginsPath).exists())
+        Assert.assertTrue("$customPluginPath directory was not created!", project.file(customPluginPath).exists())
+        Assert.assertTrue("$eclipsePluginPath directory was not created!", project.file(eclipsePluginPath).exists())
         Assert.assertTrue(
-              "$updateSiteArchivePath directory was not created!", project.file(updateSiteArchivePath).exists())
+              "artifacts.jar directory was not created!",
+              project.file(Paths.get(updateSitePath, "artifacts.jar")).exists())
+        Assert.assertTrue(
+              "content.jar directory was not created!",
+              project.file(Paths.get(updateSitePath, "content.jar")).exists())
+        Assert.assertTrue(
+              "$updateSiteArchivePath directory was not created!",
+              project.file(updateSiteArchivePath).exists())
     }
 
     private static File sourceDirectoryWithTheTestProject() {
