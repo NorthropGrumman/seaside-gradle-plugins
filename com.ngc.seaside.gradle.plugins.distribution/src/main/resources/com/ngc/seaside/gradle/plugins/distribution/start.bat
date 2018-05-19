079409ff3ae06cfb43131e09655775ef06b908d5
Tyler King <Tyler.King@ngc.com>
2018-05-18 20:47:32 -0500

    Fix felxi service default script template

--- com.ngc.seaside.gradle.plugins.distribution/src/main/resources/com/ngc/seaside/gradle/plugins/distribution/start.bat
+++ com.ngc.seaside.gradle.plugins.distribution/src/main/resources/com/ngc/seaside/gradle/plugins/distribution/start.bat
@@ -25,10 +25,10 @@ rem Set environment.
 for %%? in ("%~dp0..") do set NG_FW_HOME=%%~f?
 
 rem The system properties that are set when the JVM is started.
-set FRAMEWORK_OPTS=-DNG_FW_HOME="%NG_FW_HOME%" -Djavax.xml.accessExternalSchema=all -Dfelix.config.properties="file:%NG_FW_HOME%/platform/configuration/config.properties" __FELIX_JVM_PROPERTIES__
+set FRAMEWORK_OPTS=-DNG_FW_HOME="%NG_FW_HOME%" -Djavax.xml.accessExternalSchema=all -Dfelix.config.properties="file:%NG_FW_HOME%/platform/configuration/config.properties" ${FELIX_JVM_PROPERTIES}
 
 rem The arguments passed to Felix when it is started.
-set FELIX_OPTS="%NG_FW_HOME%/platform/cache" -b "%NG_FW_HOME%/platform" __FELIX_PROGRAM_ARGUMENTS__
+set FELIX_OPTS="%NG_FW_HOME%/platform/cache" -b "%NG_FW_HOME%/platform" ${FELIX_PROGRAM_ARGUMENTS}
 
 rem The main jar for running Felix
 for /f %%i in ('dir "%NG_FW_HOME%\\platform\\org.apache.felix.main-*.jar" /s /b') do set MAIN_JAR=%%i
