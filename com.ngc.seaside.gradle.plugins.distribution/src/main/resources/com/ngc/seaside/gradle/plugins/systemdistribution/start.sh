a8da15df39175caaadbcd9e0090a7fcb75cc5290
Cameron Chaparro <cameron.chaparro@ngc.com>
2018-07-09 15:23:36 -0500

    Update systemdistribution start script

--- com.ngc.seaside.gradle.plugins.distribution/src/main/resources/com/ngc/seaside/gradle/plugins/systemdistribution/start.sh
+++ com.ngc.seaside.gradle.plugins.distribution/src/main/resources/com/ngc/seaside/gradle/plugins/systemdistribution/start.sh
@@ -15,39 +15,8 @@
 #  or reproduction of this material is strictly forbidden unless prior written
 #  permission is obtained from Northrop Grumman.
 #
+
 currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-closedPidList=''
-
-shutdown() {
- echo ""
- echo "****************************************"
- echo "Gracefully shutting down services!"
- echo "****************************************"
-
- killProcesses $$ &>/dev/null
-
- printf "DONE\n"
- exit
-}
-
-#
-# Gracefully shuts down our services
-#
-killProcesses() {
-   closedPidList="$closedPidList $1"
-
-   local childPidList=`ps -ef | grep $1 | awk 'NR>1 {print $2}' | head -n -1`
-   local childPid
-
-    for childPid in ${childPidList[*]}
-    do
-        if [[ ${closedPidList} != ${childPid} ]]
-        then
-            killProcesses $childPid
-            kill -TERM $childPid
-        fi
-    done
-}
 
 # convert script to unix format
 find . -type f -name "*.sh" -exec sed -i 's/\x0d//g' {} \+
@@ -56,17 +25,9 @@ find . -type f -name "*.sh" -exec sed -i 's/\x0d//g' {} \+
 find ./ -name "*.sh" -exec chmod +x {} \;
 
 printf "Starting services...\n"
-for folder in `ls -d $currentDir/*/`
-do
+for folder in `ls -d $currentDir/*/`; do
    printf "Entering $folder ...\n"
-
    $folder/bin/start.sh "$@" &
 done
 
-trap 'shutdown' INT TERM KILL
-
-while true; do
-    echo -en "Press Ctrl-C to exit"
-    read input
-done
-
+wait
