dd638b3129526d704ec4059f10b4bb38e541d932
Tyler King <Tyler.King@ngc.com>
2018-07-26 10:11:47 -0500

    Refactor felix service plugin to use osgi resolver from base project

---
+++ com.ngc.seaside.gradle.plugins.distribution/src/test/java/com/ngc/seaside/gradle/plugins/distribution/SeasideServiceDistributionPluginIT.java
@@ -0,0 +1,36 @@
+package com.ngc.seaside.gradle.plugins.distribution;
+
+import com.ngc.seaside.gradle.util.TaskResolver;
+import org.gradle.api.Project;
+import org.gradle.testfixtures.ProjectBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class SeasideServiceDistributionPluginIT {
+
+   private Project project;
+   private SeasideServiceDistributionPlugin plugin;
+
+   @Before
+   public void before() {
+      project = ProjectBuilder.builder().build();
+      plugin = new SeasideServiceDistributionPlugin();
+      plugin.apply(project);
+   }
+
+   @Test
+   public void doesApplyPlugin() {
+      TaskResolver resolver = new TaskResolver(project);
+
+      assertNotNull(resolver.findTask("copyResources"));
+      assertNotNull(resolver.findTask("copyPlatformBundles"));
+      assertNotNull(resolver.findTask("copyThirdPartyBundles"));
+      assertNotNull(resolver.findTask("copyBundles"));
+      assertNotNull(resolver.findTask("tar"));
+      assertNotNull(resolver.findTask("zip"));
+      assertNotNull(resolver.findTask("buildDist"));
+   }
+
+}
