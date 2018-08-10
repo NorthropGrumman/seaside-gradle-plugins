9a6ecb9a3fc2ca1a7024272202cb7d39b403e4a2
Cameron Chaparro <cameron.chaparro@ngc.com>
2018-08-10 10:41:54 -0500

    Add initial plugin project classes
    Add DependencyLockPlugin that can be applied by projects to use dependency
    locking on their dependencies.
    
    Add ResolveAndLockAllDependenciesTask that can be run to resolve all
    dependencies and lock them at specific resolvable version numbers.

---
+++ com.ngc.seaside.gradle.plugins.dependency.lock/src/main/java/com/ngc/seaside/gradle/plugins/dependency/lock/DependencyLockPlugin.java
@@ -0,0 +1,12 @@
+package com.ngc.seaside.gradle.plugins.dependency.lock;
+
+import com.ngc.seaside.gradle.api.AbstractProjectPlugin;
+
+import org.gradle.api.Project;
+
+public class DependencyLockPlugin extends AbstractProjectPlugin {
+
+   @Override
+   protected void doApply(Project project) {
+   }
+}
---
+++ com.ngc.seaside.gradle.plugins.dependency.lock/src/main/java/com/ngc/seaside/gradle/plugins/dependency/lock/ResolveAndLockAllDependenciesTask.java
@@ -0,0 +1,11 @@
+package com.ngc.seaside.gradle.plugins.dependency.lock;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+
+public class ResolveAndLockAllDependenciesTask extends DefaultTask {
+
+   @TaskAction
+   void resolveAndLockDependencies() {
+   }
+}
