b03825be425209b1be2a42fc990afb209e6c295c
Cameron Chaparro <cameron.chaparro@ngc.com>
2018-08-10 16:06:44 -0500

    Update DependencyLockPluginIT.java
    Add test to ensure the plugin can be applied and the task is created.

--- com.ngc.seaside.gradle.plugins.dependency.lock/src/integrationTest/java/com/ngc/seaside/gradle/plugins/dependency/lock/DependencyLockPluginIT.java
+++ com.ngc.seaside.gradle.plugins.dependency.lock/src/integrationTest/java/com/ngc/seaside/gradle/plugins/dependency/lock/DependencyLockPluginIT.java
@@ -1,5 +1,54 @@
 package com.ngc.seaside.gradle.plugins.dependency.lock;
 
+import com.ngc.seaside.gradle.util.TaskResolver;
+import com.ngc.seaside.gradle.util.test.TestingUtilities;
+
+import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+
 public class DependencyLockPluginIT {
 
+   private TaskResolver resolver;
+
+   @Before
+   public void before() {
+      File projectDir = TestingUtilities.setUpTheTestProjectDirectory(
+            sourceDirectoryWithTheTestProject(),
+            pathToTheDestinationProjectDirectory()
+      );
+
+      Project project = TestingUtilities.createTheTestProjectWith(projectDir);
+
+      DependencyLockPlugin plugin = new DependencyLockPlugin();
+      plugin.doApply(project);
+
+      resolver = new TaskResolver(project);
+   }
+
+   @Test
+   public void doesApplyDependencyLockPlugin() {
+      Task task = resolver.findTask(ResolveAndLockAllDependenciesTask.TASK_NAME);
+      Assert.assertNotNull(task);
+      Assert.assertEquals("task group is incorrect!", ResolveAndLockAllDependenciesTask.TASK_GROUP, task.getGroup());
+      Assert.assertEquals("task description is incorrect!",
+                          ResolveAndLockAllDependenciesTask.TASK_DESCRIPTION,
+                          task.getDescription());
+   }
+
+   private static File sourceDirectoryWithTheTestProject() {
+      return TestingUtilities.turnListIntoPath(
+            "src", "integrationTest", "resources", "sealion-java-hello-world-monorepo"
+      );
+   }
+
+   private static File pathToTheDestinationProjectDirectory() {
+      return TestingUtilities.turnListIntoPath(
+            "build", "integrationTest", "resources", "dependency-lock", "sealion-java-hello-world-monorepo"
+      );
+   }
 }
