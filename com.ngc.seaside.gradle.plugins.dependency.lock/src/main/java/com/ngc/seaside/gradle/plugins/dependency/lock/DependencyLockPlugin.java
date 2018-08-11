7095cdf154c8ae6c3e1c00d5d5fbaa1bb31fa3bb
Cameron Chaparro <cameron.chaparro@ngc.com>
2018-08-10 20:01:14 -0500

    Add implementations
    Add an implementation of DependencyLockPlugin based that will activate
    dependency locking for each configuration and will create / configure the
    resolveAndLockAllDependencies task.
    
    Add an implementation of ResolveAndLockAllDependenciesTask that will ensure
    --write-locks was passed when the task is called and, if it was, will resolve
    and write lock files for each of the configurations.
    
    Note: While I everything suggests this should be working, it's not
    because (apparently) the plugin can't be found. I'll have to look into fixing
    this next.

--- com.ngc.seaside.gradle.plugins.dependency.lock/src/main/java/com/ngc/seaside/gradle/plugins/dependency/lock/DependencyLockPlugin.java
+++ com.ngc.seaside.gradle.plugins.dependency.lock/src/main/java/com/ngc/seaside/gradle/plugins/dependency/lock/DependencyLockPlugin.java
@@ -4,9 +4,20 @@ import com.ngc.seaside.gradle.api.AbstractProjectPlugin;
 
 import org.gradle.api.Project;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DependencyLockPlugin extends AbstractProjectPlugin {
 
    @Override
    protected void doApply(Project project) {
+      project.getConfigurations().forEach(c -> c.getResolutionStrategy().activateDependencyLocking());
+
+      Map<String, String> options = new HashMap<>();
+      options.put("name", ResolveAndLockAllDependenciesTask.TASK_NAME);
+      options.put("group", ResolveAndLockAllDependenciesTask.TASK_GROUP);
+      options.put("description", ResolveAndLockAllDependenciesTask.TASK_DESCRIPTION);
+
+      project.getTasks().create(options);
    }
 }
--- com.ngc.seaside.gradle.plugins.dependency.lock/src/main/java/com/ngc/seaside/gradle/plugins/dependency/lock/ResolveAndLockAllDependenciesTask.java
+++ com.ngc.seaside.gradle.plugins.dependency.lock/src/main/java/com/ngc/seaside/gradle/plugins/dependency/lock/ResolveAndLockAllDependenciesTask.java
@@ -1,11 +1,27 @@
 package com.ngc.seaside.gradle.plugins.dependency.lock;
 
 import org.gradle.api.DefaultTask;
+import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.tasks.TaskInstantiationException;
 
 public class ResolveAndLockAllDependenciesTask extends DefaultTask {
 
+   public static final String TASK_GROUP = "Dependency Lock";
+   public static final String TASK_NAME = "resolveAndLockAllDependencies";
+   public static final String TASK_DESCRIPTION = "Resolve all dependencies and write a lock file containing their GAVs";
+
    @TaskAction
-   void resolveAndLockDependencies() {
+   public void resolveAndLockAllDependencies() {
+      doFirst(__ -> {
+         if (!getProject().getGradle().getStartParameter().isWriteDependencyLocks()) {
+            throw new TaskInstantiationException(String.format("%s task must be run with --write-locks",
+                                                               ResolveAndLockAllDependenciesTask.TASK_NAME));
+         }
+      });
+
+      doLast(__ -> getProject().getConfigurations()
+                               .matching(Configuration::isCanBeResolved)
+                               .all(Configuration::resolve));
    }
 }
