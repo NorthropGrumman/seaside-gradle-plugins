f547494a6f0f5f1c2f9d5d2f527aa9bf4d5911e6
Cameron Chaparro <cameron.chaparro@ngc.com>
2018-08-15 18:53:42 -0500

    Not working: Update DependencyLockPlugin.java
    Initial attempt at getting dependency locking to ignore `com.ngc.seaside'
    products.

--- com.ngc.seaside.gradle.plugins.dependency.lock/src/main/java/com/ngc/seaside/gradle/plugins/dependency/lock/DependencyLockPlugin.java
+++ com.ngc.seaside.gradle.plugins.dependency.lock/src/main/java/com/ngc/seaside/gradle/plugins/dependency/lock/DependencyLockPlugin.java
@@ -3,6 +3,8 @@ package com.ngc.seaside.gradle.plugins.dependency.lock;
 import com.ngc.seaside.gradle.api.AbstractProjectPlugin;
 
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.ComponentSelection;
+import org.gradle.api.artifacts.ResolutionStrategy;
 
 public class DependencyLockPlugin extends AbstractProjectPlugin {
 
@@ -11,7 +13,7 @@ public class DependencyLockPlugin extends AbstractProjectPlugin {
 
    @Override
    protected void doApply(Project project) {
-      project.getConfigurations().forEach(c -> c.getResolutionStrategy().activateDependencyLocking());
+      project.getConfigurations().forEach(c -> c.resolutionStrategy(this::configureResolutionStrategy));
 
       project.getTasks().create(
             RESOLVE_AND_LOCK_DEPENDENCIES_TASK_NAME,
@@ -20,5 +22,17 @@ public class DependencyLockPlugin extends AbstractProjectPlugin {
                task.setGroup(DEPENDENCY_LOCK_PLUGIN_GROUP);
                task.setDescription(ResolveAndLockAllDependenciesTask.DESCRIPTION);
             });
+   }
+
+   private void configureResolutionStrategy(ResolutionStrategy strategy) {
+      strategy.activateDependencyLocking();
+      strategy.preferProjectModules();
+      strategy.componentSelection(rules -> rules.all(this::rejectLockingCompanyComponents));
+   }
+
+   private void rejectLockingCompanyComponents(ComponentSelection selection) {
+      if (selection.getCandidate().getGroup().startsWith("com.ngc.seaside")) {
+         selection.reject("don't include CEACIDE products");
+      }
    }
 }
