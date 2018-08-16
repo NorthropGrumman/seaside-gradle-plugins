3c58b42e9e599ccd5f02d2bb97848aad0fdef6d8
Cameron Chaparro <cameron.chaparro@ngc.com>
2018-08-16 09:59:31 -0500

    Revert "Not working: Update DependencyLockPlugin.java"
    This reverts commit f547494a6f0f5f1c2f9d5d2f527aa9bf4d5911e6.

--- com.ngc.seaside.gradle.plugins.dependency.lock/src/main/java/com/ngc/seaside/gradle/plugins/dependency/lock/DependencyLockPlugin.java
+++ com.ngc.seaside.gradle.plugins.dependency.lock/src/main/java/com/ngc/seaside/gradle/plugins/dependency/lock/DependencyLockPlugin.java
@@ -3,8 +3,6 @@ package com.ngc.seaside.gradle.plugins.dependency.lock;
 import com.ngc.seaside.gradle.api.AbstractProjectPlugin;
 
 import org.gradle.api.Project;
-import org.gradle.api.artifacts.ComponentSelection;
-import org.gradle.api.artifacts.ResolutionStrategy;
 
 public class DependencyLockPlugin extends AbstractProjectPlugin {
 
@@ -13,7 +11,7 @@ public class DependencyLockPlugin extends AbstractProjectPlugin {
 
    @Override
    protected void doApply(Project project) {
-      project.getConfigurations().forEach(c -> c.resolutionStrategy(this::configureResolutionStrategy));
+      project.getConfigurations().forEach(c -> c.getResolutionStrategy().activateDependencyLocking());
 
       project.getTasks().create(
             RESOLVE_AND_LOCK_DEPENDENCIES_TASK_NAME,
@@ -22,17 +20,5 @@ public class DependencyLockPlugin extends AbstractProjectPlugin {
                task.setGroup(DEPENDENCY_LOCK_PLUGIN_GROUP);
                task.setDescription(ResolveAndLockAllDependenciesTask.DESCRIPTION);
             });
-   }
-
-   private void configureResolutionStrategy(ResolutionStrategy strategy) {
-      strategy.activateDependencyLocking();
-      strategy.preferProjectModules();
-      strategy.componentSelection(rules -> rules.all(this::rejectLockingCompanyComponents));
-   }
-
-   private void rejectLockingCompanyComponents(ComponentSelection selection) {
-      if (selection.getCandidate().getGroup().startsWith("com.ngc.seaside")) {
-         selection.reject("don't include CEACIDE products");
-      }
    }
 }
