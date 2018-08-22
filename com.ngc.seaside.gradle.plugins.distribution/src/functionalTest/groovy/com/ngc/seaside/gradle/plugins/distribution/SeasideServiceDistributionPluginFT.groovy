4b7efb8911059fb41c7637263dffa58a16a5dd91
Cameron Chaparro <cameron.chaparro@ngc.com>
2018-08-22 11:43:25 -0500

    Update SeasideServiceDistributionPluginFT.groovy
    Run auto-formatter.

--- com.ngc.seaside.gradle.plugins.distribution/src/functionalTest/groovy/com/ngc/seaside/gradle/plugins/distribution/SeasideServiceDistributionPluginFT.groovy
+++ com.ngc.seaside.gradle.plugins.distribution/src/functionalTest/groovy/com/ngc/seaside/gradle/plugins/distribution/SeasideServiceDistributionPluginFT.groovy
@@ -1,7 +1,6 @@
 package com.ngc.seaside.gradle.plugins.distribution
 
 import com.ngc.seaside.gradle.util.test.SeasideGradleRunner
-
 import org.apache.commons.io.FileUtils
 import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
@@ -17,35 +16,35 @@ import java.nio.file.Paths
 
 class SeasideServiceDistributionPluginFT {
 
-    private File projectDir
-    private Project project
+   private File projectDir
+   private Project project
 
-    @Before
-    void before() {
-        File source = Paths.get("src/functionalTest/resources/distribution/com.ngc.example.distribution").toFile()
-        Path targetPath = Paths.get("build/functionalTest/distribution/com.ngc.example.distribution")
-        projectDir = Files.createDirectories(targetPath).toFile()
-        FileUtils.copyDirectory(source, projectDir)
+   @Before
+   void before() {
+      File source = Paths.get("src/functionalTest/resources/distribution/com.ngc.example.distribution").toFile()
+      Path targetPath = Paths.get("build/functionalTest/distribution/com.ngc.example.distribution")
+      projectDir = Files.createDirectories(targetPath).toFile()
+      FileUtils.copyDirectory(source, projectDir)
 
-        project = ProjectBuilder.builder().withProjectDir(projectDir).build()
-    }
+      project = ProjectBuilder.builder().withProjectDir(projectDir).build()
+   }
 
 
-    @Test
-    void doesRunGradleBuildWithSuccess() {
-        BuildResult result = SeasideGradleRunner.create().withProjectDir(project.projectDir)
-              .withNexusProperties()
-              .withPluginClasspath()
-              .forwardOutput()
-              .withArguments("clean", "build")
-              .build()
+   @Test
+   void doesRunGradleBuildWithSuccess() {
+      BuildResult result = SeasideGradleRunner.create().withProjectDir(project.projectDir)
+            .withNexusProperties()
+            .withPluginClasspath()
+            .forwardOutput()
+            .withArguments("clean", "build")
+            .build()
 
-        Assert.assertEquals(TaskOutcome.valueOf("SUCCESS"), result.task(":build").getOutcome())
+      Assert.assertEquals(TaskOutcome.valueOf("SUCCESS"), result.task(":build").getOutcome())
 
-        Assert.assertTrue("did not create ZIP!",
-                          Files.isRegularFile(projectDir.toPath().resolve(Paths.get(
-                                "build",
-                                "distribution",
-                                "com.ngc.seaside.example.distribution-1.0-SNAPSHOT.zip"))))
-    }
+      Assert.assertTrue("did not create ZIP!",
+                        Files.isRegularFile(projectDir.toPath().resolve(Paths.get(
+                              "build",
+                              "distribution",
+                              "com.ngc.seaside.example.distribution-1.0-SNAPSHOT.zip"))))
+   }
 }
