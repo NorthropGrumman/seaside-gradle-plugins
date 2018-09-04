56b854ec05bc7715e524c44f61d342539cbaed29
ThomasJHooper <thomas.hooper@ngc.com>
2018-09-04 12:27:56 -0500

    Update licenses.

--- com.ngc.seaside.gradle.plugins.distribution/src/main/resources/com/ngc/seaside/gradle/plugins/systemdistribution/start.bat
+++ com.ngc.seaside.gradle.plugins.distribution/src/main/resources/com/ngc/seaside/gradle/plugins/systemdistribution/start.bat
@@ -1,18 +1,18 @@
 @REM
+@REM UNCLASSIFIED
+@REM Northrop Grumman Proprietary
+@REM ____________________________
 @REM
-@REM  Northrop Grumman Proprietary
-@REM  ____________________________
+@REM Copyright (C) 2018, Northrop Grumman Systems Corporation
+@REM All Rights Reserved.
 @REM
-@REM   Copyright (C) 2018, Northrop Grumman Systems Corporation
-@REM   All Rights Reserved.
-@REM
-@REM  NOTICE:  All information contained herein is, and remains the property of
-@REM  Northrop Grumman Systems Corporation. The intellectual and technical concepts
-@REM  contained herein are proprietary to Northrop Grumman Systems Corporation and
-@REM  may be covered by U.S. and Foreign Patents or patents in process, and are
-@REM  protected by trade secret or copyright law. Dissemination of this information
-@REM  or reproduction of this material is strictly forbidden unless prior written
-@REM  permission is obtained from Northrop Grumman.
+@REM NOTICE:  All information contained herein is, and remains the property of
+@REM Northrop Grumman Systems Corporation. The intellectual and technical concepts
+@REM contained herein are proprietary to Northrop Grumman Systems Corporation and
+@REM may be covered by U.S. and Foreign Patents or patents in process, and are
+@REM protected by trade secret or copyright law. Dissemination of this information
+@REM or reproduction of this material is strictly forbidden unless prior written
+@REM permission is obtained from Northrop Grumman.
 @REM
 
 @echo off
