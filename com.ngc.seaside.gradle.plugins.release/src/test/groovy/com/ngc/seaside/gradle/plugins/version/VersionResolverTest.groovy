4b9b669356620a4a5cc4378c61df376e23393adb
Cameron Chaparro <cameron.chaparro@ngc.com>
2018-09-14 12:46:33 -0500

    Update VersionResolverTest.groovy
    Use static imports for assertions and `mock', `when', etc.
    
    Run IntelliJ auto-formatter.

--- com.ngc.seaside.gradle.plugins.release/src/test/groovy/com/ngc/seaside/gradle/plugins/version/VersionResolverTest.groovy
+++ com.ngc.seaside.gradle.plugins.release/src/test/groovy/com/ngc/seaside/gradle/plugins/version/VersionResolverTest.groovy
@@ -18,58 +18,63 @@ package com.ngc.seaside.gradle.plugins.version
 
 import org.gradle.api.Project
 import org.gradle.api.logging.Logger
-import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
-import org.mockito.ArgumentMatchers
 import org.mockito.Mockito
 
+import static org.junit.Assert.assertEquals
+import static org.junit.Assert.assertNotNull
+import static org.mockito.ArgumentMatchers.anyString
+import static org.mockito.Mockito.mock
+import static org.mockito.Mockito.when
+
 class VersionResolverTest {
-   private Project project = Mockito.mock(Project, Mockito.RETURNS_DEEP_STUBS)
-   private Project rootProject = Mockito.mock(Project)
-   private Logger logger = Mockito.mock(Logger)
+
+   private Project project = mock(Project, Mockito.RETURNS_DEEP_STUBS)
+   private Project rootProject = mock(Project)
+   private Logger logger = mock(Logger)
    private VersionResolver resolver
-   private final File file = Mockito.mock(File)
+   private final File file = mock(File)
 
    @Before
    void before() {
-      Mockito.when(project.version).thenReturn("1.2.3.DEV")
-      Mockito.when(project.rootProject).thenReturn(rootProject)
-      Mockito.when(project.extensions.findByType(VersionResolver)).thenReturn(null)
-      Mockito.when(project.getLogger()).thenReturn(logger)
-      Mockito.when(rootProject.buildFile).thenReturn(file)
-      Mockito.when(rootProject.projectDir).thenReturn(file)
-      Mockito.when(rootProject.projectDir.parent).thenReturn("")
+      when(project.version).thenReturn("1.2.3.DEV")
+      when(project.rootProject).thenReturn(rootProject)
+      when(project.extensions.findByType(VersionResolver)).thenReturn(null)
+      when(project.getLogger()).thenReturn(logger)
+      when(rootProject.buildFile).thenReturn(file)
+      when(rootProject.projectDir).thenReturn(file)
+      when(rootProject.projectDir.parent).thenReturn("")
       resolver = new VersionResolver(project)
    }
 
    @Test
    void doesGetSemanticVersionFromString() {
-      Assert.assertEquals("1.0.0-SNAPSHOT", resolver.getSemanticVersion("version = 1.0.0-SNAPSHOT"))
-      Assert.assertEquals("1.0.3.RC", resolver.getSemanticVersion("version = 1.0.3.RC"))
-      Assert.assertEquals("1.2.0-TEST", resolver.getSemanticVersion("version =   1.2.0-TEST"))
-      Assert.assertEquals("1.2.7-SNAPSHOT", resolver.getSemanticVersion("  version= 1.2.7-SNAPSHOT"))
-      Assert.assertEquals("1.5.7-SNAPSHOT", resolver.getSemanticVersion("version=1.5.7-SNAPSHOT"))
+      assertEquals("1.0.0-SNAPSHOT", resolver.getSemanticVersion("version = 1.0.0-SNAPSHOT"))
+      assertEquals("1.0.3.RC", resolver.getSemanticVersion("version = 1.0.3.RC"))
+      assertEquals("1.2.0-TEST", resolver.getSemanticVersion("version =   1.2.0-TEST"))
+      assertEquals("1.2.7-SNAPSHOT", resolver.getSemanticVersion("  version= 1.2.7-SNAPSHOT"))
+      assertEquals("1.5.7-SNAPSHOT", resolver.getSemanticVersion("version=1.5.7-SNAPSHOT"))
    }
 
    @Test
    void doesGetDefaultVersionFileName() {
-      Assert.assertNotNull(resolver.versionFile)
-      Assert.assertEquals(file, resolver.versionFile)
+      assertNotNull(resolver.versionFile)
+      assertEquals(file, resolver.versionFile)
    }
 
    @Test
    void doesGetCustomVersionFileName() {
-      Mockito.when(project.hasProperty(ArgumentMatchers.anyString())).thenReturn(true)
-      Mockito.when(project.property(ArgumentMatchers.anyString())).thenReturn("custom.gradle")
+      when(project.hasProperty(anyString())).thenReturn(true)
+      when(project.property(anyString())).thenReturn("custom.gradle")
       resolver = new VersionResolver(project)
 
-      Assert.assertNotNull(resolver.versionFile)
-      Assert.assertEquals(file, resolver.versionFile)
+      assertNotNull(resolver.versionFile)
+      assertEquals(file, resolver.versionFile)
    }
 
    @Test
    void doesGetTagNameWithPrefix() {
-      Assert.assertEquals("myPrefix_1.2.3", resolver.getTagName("myPrefix_", ".DEV"))
+      assertEquals("myPrefix_1.2.3", resolver.getTagName("myPrefix_", ".DEV"))
    }
 }
