07719adcf0ee5e43d03f6206dfd89ae1b282bc3c
ThomasJHooper <thomas.hooper@ngc.com>
2018-10-26 11:35:13 -0500

    Remove check for java-gradle-plugin from seaside maven plugin.

--- com.ngc.seaside.gradle.plugins.repository/src/main/java/com/ngc/seaside/gradle/plugins/maven/SeasideMavenPlugin.java
+++ com.ngc.seaside.gradle.plugins.repository/src/main/java/com/ngc/seaside/gradle/plugins/maven/SeasideMavenPlugin.java
@@ -27,7 +27,6 @@ import org.gradle.api.publish.maven.MavenPublication;
 import org.gradle.api.publish.maven.plugins.MavenPublishPlugin;
 import org.gradle.api.publish.plugins.PublishingPlugin;
 import org.gradle.api.tasks.TaskContainer;
-import org.gradle.plugin.devel.plugins.JavaGradlePluginPlugin;
 
 /**
  * Convenience plugin for configuring the {@link MavenPublishPlugin maven-publish plugin}. This plugin will prevent
@@ -53,16 +52,14 @@ public class SeasideMavenPlugin extends AbstractProjectPlugin {
       // Do not do this logic if the MavenPublishPluginPlugin is applied.  This plugin is applied by the
       // java-gradle-plugin.  If we don't do this, multiple publications with the same GAVs are configured which
       // results in issues during uploads of releases.
-      if (!project.getPlugins().hasPlugin(JavaGradlePluginPlugin.class)) {
-         project.getPlugins().withType(JavaPlugin.class, plugin -> {
-            PublishingExtension extension = project.getExtensions().getByType(PublishingExtension.class);
-            extension.publications(publications -> {
-               publications.create(MAVEN_JAVA_PUBLICATION_NAME, MavenPublication.class, publication -> {
-                  publication.from(project.getComponents().getByName("java"));
-               });
+      project.getPlugins().withType(JavaPlugin.class, plugin -> {
+         PublishingExtension extension = project.getExtensions().getByType(PublishingExtension.class);
+         extension.publications(publications -> {
+            publications.create(MAVEN_JAVA_PUBLICATION_NAME, MavenPublication.class, publication -> {
+               publication.from(project.getComponents().getByName("java"));
             });
          });
-      }
+      });
    }
 
    private void configureTasks(Project project) {
