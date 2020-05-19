54f3762519b520dd4c963ecb4953325db77ebc2f
JD Black <jondavid.black@ngc.com>
2020-05-18 19:02:01 -0500

    Changed license to MIT open source

--- com.ngc.seaside.gradle.plugins.command/src/functionalTest/resources/sealion-java-hello-world/com.ngc.seaside.service.konnichiwamojuru/src/main/templates/bar/templateContent/gradlew.bat
+++ com.ngc.seaside.gradle.plugins.command/src/functionalTest/resources/sealion-java-hello-world/com.ngc.seaside.service.konnichiwamojuru/src/main/templates/bar/templateContent/gradlew.bat
@@ -1,18 +1,24 @@
 @REM
 @REM UNCLASSIFIED
-@REM Northrop Grumman Proprietary
-@REM ____________________________
 @REM
-@REM Copyright (C) 2019, Northrop Grumman Systems Corporation
-@REM All Rights Reserved.
+@REM Copyright 2020 Northrop Grumman Systems Corporation
 @REM
-@REM NOTICE:  All information contained herein is, and remains the property of
-@REM Northrop Grumman Systems Corporation. The intellectual and technical concepts
-@REM contained herein are proprietary to Northrop Grumman Systems Corporation and
-@REM may be covered by U.S. and Foreign Patents or patents in process, and are
-@REM protected by trade secret or copyright law. Dissemination of this information
-@REM or reproduction of this material is strictly forbidden unless prior written
-@REM permission is obtained from Northrop Grumman.
+@REM Permission is hereby granted, free of charge, to any person obtaining a copy of
+@REM this software and associated documentation files (the "Software"), to deal in
+@REM the Software without restriction, including without limitation the rights to use,
+@REM copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+@REM Software, and to permit persons to whom the Software is furnished to do so,
+@REM subject to the following conditions:
+@REM
+@REM The above copyright notice and this permission notice shall be included in
+@REM all copies or substantial portions of the Software.
+@REM
+@REM THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+@REM INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+@REM PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+@REM HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+@REM OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+@REM SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 @REM
 
 @if "%DEBUG%" == "" @echo off
